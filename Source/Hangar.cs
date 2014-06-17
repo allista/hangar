99c31b05fc4a42e7a68ae4a966a322ac01233440
Allis Tauri <allista@gmail.com>
2014-06-17 19:30:50 +0400

    Changed volume and dimensions calculation algorithm.

--- Source/Hangar.cs
+++ Source/Hangar.cs
@@ -24,29 +24,30 @@ namespace AtHangar
 		}
 		
 		//internal properties
-		private IHangarAnimator hangar_gates;
+		private BaseHangarAnimator hangar_gates;
 		public HangarGates gates_state { get { return hangar_gates.GatesState; } }
 		public HangarState hangar_state { get; private set;}
-		private double total_volume = 0;
-		private double usefull_volume_ratio = 0.7; //only 70% of the volume may be used by docking vessels
-		private double crew_volume_ratio = 0.3; //only 30% of the remaining volyme may be used for crew (i.e. V*(1-usefull_r)*crew_r)
-		private double volume_per_kerbal = 1.3; //m^3
+		public Metric hangar_metric;
+		private float usefull_volume_ratio = 0.7f; //only 70% of the volume may be used by docking vessels
+		private float crew_volume_ratio    = 0.3f; //only 30% of the remaining volyme may be used for crew (i.e. V*(1-usefull_r)*crew_r)
+		private float volume_per_kerbal    = 1.5f; //m^3
 		//persistent private fields
-		[KSPField (isPersistant = true)] private double used_volume = 0;
-		[KSPField (isPersistant = true)] private float base_mass    = 0;
-		[KSPField (isPersistant = true)] private float vessels_mass = 0;
+		[KSPField (isPersistant = true)] private float used_volume  = 0f;
+		[KSPField (isPersistant = true)] private float base_mass    = 0f;
+		[KSPField (isPersistant = true)] private float vessels_mass = 0f;
 		
 		//vessels
 		private Dictionary<Guid, StoredVessel> stored_vessels = new Dictionary<Guid, StoredVessel>();
 		private Dictionary<Guid, bool> probed_ids = new Dictionary<Guid, bool>();
 		
 		//vessel spawn
-		[KSPField (isPersistant = false)] public float  SpawnHeightOffset = 0.0f;
-		[KSPField (isPersistant = false)] public string SpawnTransform;
+		[KSPField (isPersistant = false)] public float  LaunchHeightOffset = 0.0f;
+		[KSPField (isPersistant = false)] public string LaunchTransform;
+		[KSPField (isPersistant = false)] public string HangarSpace;
 		Transform restoreTransform;
 		
 		//gui fields
-		[KSPField (guiName = "Volume", guiActive = true, guiActiveEditor=true)] public string total_v;
+		[KSPField (guiName = "Volume", guiActive = true, guiActiveEditor=true)] public string hangar_v;
 		[KSPField (guiName = "Volume used", guiActive = true)] public string used_v;
 		[KSPField (guiName = "Vessels docked", guiActive = true)] public string vessels_docked;
 		[KSPField (guiName = "Crew capacity", guiActive = true, guiActiveEditor=true)] public string crew_capacity;
@@ -83,6 +84,13 @@ namespace AtHangar
 		public int numVessels() { return stored_vessels.Count; }
 		
 		
+		public override void OnAwake()
+		{
+			base.OnAwake ();
+			usefull_volume_ratio = (float)Math.Pow(usefull_volume_ratio, 1/3f);
+		}
+		
+		
 		//all initialization goes here instead of the constructor as documented in Unity API
 		public override void OnStart(StartState state)
 		{
@@ -94,11 +102,11 @@ namespace AtHangar
 			if(state == StartState.Editor) return;
 			//if not in editor, initialize Animator
 			part.force_activate();
-            hangar_gates = part.Modules.OfType<IHangarAnimator>().SingleOrDefault();
+            hangar_gates = part.Modules.OfType<BaseHangarAnimator>().SingleOrDefault();
 			if (hangar_gates == null)
 			{
-                hangar_gates = new DummyHangarAnimator();
-				Debug.Log("[Hangar] Using DummyHangarAnimator");
+                hangar_gates = new BaseHangarAnimator();
+				Debug.Log("[Hangar] Using BaseHangarAnimator");
 			}
             else
             {
@@ -115,50 +123,88 @@ namespace AtHangar
 			part.mass = base_mass+vessels_mass; 
 		}
 		
-		//calculate part volume as the volume of the biggest of bounding boxes of model meshes
-		public static double PartVolume(Part p)
-		{
-			if(p == null) return 0;
-			var model = p.FindModelTransform("model");
-			if (model == null) return 0;
-			float V_scale = model.localScale.x*model.localScale.y*model.localScale.z;
-			double vol = 0;
-			foreach(MeshFilter m in p.FindModelComponents<MeshFilter>())
-			{
-				Vector3 s = Vector3.Scale(m.mesh.bounds.size, m.transform.localScale);
-				double mvol = s.x*s.y*s.z*V_scale;
-				if(mvol > vol) vol = mvol;
-			}
-			return vol;
-		}
-		
 		public void RecalculateVolume()
 		{
 			//recalculate total volume
-			double part_V =PartVolume(part);
-			total_volume = part_V*usefull_volume_ratio;
-			total_v = Utils.formatVolume(total_volume);
+			Metric part_metric = new Metric(part);
+			if(HangarSpace != "") 
+				hangar_metric = new Metric(part, HangarSpace);
+			if(hangar_metric == null || hangar_metric.empy())
+				hangar_metric = part_metric*usefull_volume_ratio;
 			//calculate crew capacity from remaining volume
-			part.CrewCapacity = (int)(part_V*(1-usefull_volume_ratio)*crew_volume_ratio/volume_per_kerbal);
-			crew_capacity = part.CrewCapacity.ToString();
+			if(part.Modules.OfType<HangarPartResizer>().SingleOrDefault() != null)
+			{
+				part.CrewCapacity = (int)((part_metric.volume-hangar_metric.volume)*crew_volume_ratio/volume_per_kerbal);
+				crew_capacity = part.CrewCapacity.ToString();
+			}
+			//calculate hangar volume
+			hangar_v = Utils.formatVolume(hangar_metric.volume);
 		}
 		
 		//calculate transform of restored vessel
 		private Transform GetRestoreTransform ()
 		{
-			if (SpawnTransform != "")
-				restoreTransform = part.FindModelTransform(SpawnTransform);
-			else
+			restoreTransform = null;
+			if(LaunchTransform != "")
+				restoreTransform = part.FindModelTransform(LaunchTransform);
+			if(restoreTransform == null)
 			{
-				Vector3 offset = Vector3.up * SpawnHeightOffset;
+				Vector3 offset = Vector3.up * LaunchHeightOffset;
 				Transform t = part.transform;
 				GameObject restorePos = new GameObject ();
 				restorePos.transform.position = t.position;
 				restorePos.transform.position += t.TransformDirection (offset);
 				restorePos.transform.rotation = t.rotation;
 				restoreTransform = restorePos.transform;
+				Debug.Log(string.Format("LaunchTransform not found. Using offset."));
 			}
+			Debug.Log(string.Format("LaunchTransform used: {0}, {1}-{2}",restoreTransform.name, restoreTransform.position,  restoreTransform.rotation.eulerAngles));
 			return restoreTransform;
+		}
+		
+		//dock launched vessel to the hangar to prevent its krakenization 
+		private void coupleVessel(Vessel vsl)
+		{
+			Part root = vsl.rootPart;
+			DockedVesselInfo vesselInfo = new DockedVesselInfo ();
+			vesselInfo.name = vsl.vesselName;
+			vesselInfo.vesselType = vsl.vesselType;
+			vesselInfo.rootPartUId = root.flightID;
+			root.Couple(part);
+			if (vessel != FlightGlobals.ActiveVessel)
+				FlightGlobals.ForceSetActiveVessel(vessel);
+		}
+		
+		//position launched vessel part by part
+		private IEnumerator<YieldInstruction>captureVessel(Vessel vsl, Vector3 offset)
+		{
+			Vector3 pos;
+			Debug.Log("1");
+			while(true) 
+			{
+				bool partsInitialized = true;
+				foreach (Part p in vsl.parts) 
+				{
+					if (!p.started)
+					{
+						partsInitialized = false;
+						break;
+					}
+				}
+				pos = restoreTransform.TransformPoint(offset);
+				vsl.SetPosition(pos, true);
+				if (partsInitialized) break;
+				OrbitPhysicsManager.HoldVesselUnpack(2);
+				Debug.Log("2");
+				yield return null;
+			}
+			Debug.Log("3");
+			FlightGlobals.overrideOrbit = false;
+			Debug.Log("4");
+			vsl.GoOffRails ();
+			Debug.Log("5");
+			coupleVessel(vsl);
+			Debug.Log("6");
 		}
 		
 		//set vessel orbit, transform, coordinates
@@ -167,8 +213,15 @@ namespace AtHangar
 			//state
 			pv.splashed  = vessel.Landed;
 			pv.landed    = vessel.Splashed;
+			pv.orbitSnapShot = new OrbitSnapshot(vessel.orbit);
 			//rotation
-			pv.rotation  = restoreTransform.rotation; //TODO: is this correct?
+			Debug.Log(string.Format("[Hangar] part position: {0}", part.transform.position));
+			Debug.Log(string.Format("[Hangar] part rotation: {0}", part.transform.rotation.eulerAngles));
+			Debug.Log(string.Format("[Hangar] old position: {0}", pv.position));
+			Debug.Log(string.Format("[Hangar] old rotation: {0}", pv.rotation.eulerAngles));
+			pv.rotation = restoreTransform.rotation; //TODO: is this correct?
+			//Debug.Log(string.Format("[Hangar] new position: {0}", pv.position));
+			//Debug.Log(string.Format("[Hangar] new rotation: {0}", pv.rotation.eulerAngles));
 			//surface
 			if(vessel.LandedOrSplashed)
 			{
@@ -178,51 +231,65 @@ namespace AtHangar
 				pv.latitude  = vessel.mainBody.GetLatitude(v);
 				pv.altitude  = vessel.mainBody.GetAltitude(v);
 			}
+			else pv.position = restoreTransform.position;
+		}
+		
+		private void PositionVessel(Vessel vsl)
+		{
+			//state
+			vsl.Splashed  = false;//vessel.Landed;
+			vsl.Landed    = false;//vessel.Splashed;
+			//rotation
+			Debug.Log(string.Format("[Hangar] part rotation: {0}", part.transform.rotation.eulerAngles));
+			Debug.Log(string.Format("[Hangar] part position: {0}", part.transform.position));
+			Debug.Log(string.Format("[Hangar] old rotation: {0}", vsl.transform.rotation.eulerAngles));
+			Debug.Log(string.Format("[Hangar] old position: {0}", vsl.transform.position));
+			vsl.vesselTransform.rotation = restoreTransform.rotation; //TODO: is this correct?
+			Debug.Log(string.Format("[Hangar] new rotation: {0}", vsl.transform.rotation.eulerAngles));
+			Debug.Log(string.Format("[Hangar] new position: {0}", vsl.transform.position));
+			//surface
+			if(vessel.LandedOrSplashed)
+			{
+				Vector3 v  = restoreTransform.position;
+				v = new Vector3d(v.x, v.y, v.z);
+				vsl.longitude = vessel.mainBody.GetLongitude(v);
+				vsl.latitude  = vessel.mainBody.GetLatitude(v);
+				vsl.altitude  = vessel.mainBody.GetAltitude(v);
+			}
 			else
 			{
 				//transform
-				pv.position = part.partTransform.InverseTransformPoint(restoreTransform.position);
-				pv.rotation = restoreTransform.rotation;
+//				vsl.vesselTransform = restoreTransform;
 				//orbit
-				pv.orbitSnapShot = new OrbitSnapshot(vessel.orbit);
+				vsl.orbitDriver.SetOrbitMode(OrbitDriver.UpdateMode.UPDATE);
+				vsl.orbit.UpdateFromOrbitAtUT(vessel.orbit, Planetarium.GetUniversalTime(), vessel.mainBody);
 			}
-		}
-		
-		//calculate approximate volume of a vessel 
-		//as the sum of volumes of bounding boxes of its parts
-		public static double VesselVolume(Vessel vsl)
-		{
-			if(vsl == null) return 0;
-			double vol = 0;
-			foreach (Part p in vsl.parts)
-				vol += PartVolume(p);
-			return vol;
 		}
 		
 		//if a vessel can be stored in the hangar
 		public bool CanStore(Vessel vsl)
 		{
-			if(vsl == null) return false;
+			if(vsl == null || vsl == vessel) return false;
 			//check if the vessel is EVA. Maybe get EVA on board too?
-			if(vsl.isEVA) 
-			{
-				FlightScreenMessager.showMessage("Kerbals cannot be docked", 3);
-				return false;
-			}
+			if(vsl.isEVA) return false;
 			//check vessel crew
 			if(vsl.GetCrewCount() > vessel.GetCrewCapacity()-vessel.GetCrewCount())
 			{
 				FlightScreenMessager.showMessage("Not enough space for the crew of a docking vessel", 3);
 				return false;
 			}
-			//check vessel volume
-			double vol = VesselVolume(vsl);
-			if(vol <= 0) return false;
-			if(vol > total_volume-used_volume) 
+			//check vessel metrics
+			Metric metric = new Metric(vsl);
+			if(metric.volume > hangar_metric.volume-used_volume) 
 			{
 				FlightScreenMessager.showMessage("There's no room in the hangar for this vessel", 3);
 				return false;
 			}
+			if(!metric.fits(hangar_metric))
+			{
+				FlightScreenMessager.showMessage("The vessel does not fit into this hangar", 3);
+				return false;
+			}	
 			return true;
 		}
 		
@@ -268,7 +335,7 @@ namespace AtHangar
 			//if hangar is not ready, return
 			if(hangar_state == HangarState.Busy) 
 			{
-				FlightScreenMessager.showMessage("Prepare hangar first.", 3);
+				FlightScreenMessager.showMessage("Prepare hangar first", 3);
 				return;
 			}
 			//check self state first
@@ -276,7 +343,7 @@ namespace AtHangar
 			{
 			case ClearToSaveStatus.NOT_WHILE_ABOUT_TO_CRASH:
 			{
-				FlightScreenMessager.showMessage("Cannot accept the vessel while about to crush.", 3);
+				FlightScreenMessager.showMessage("Cannot accept the vessel while about to crush", 3);
 				return;
 			}
 			default:
@@ -290,6 +357,9 @@ namespace AtHangar
 				probed_ids.Add(vsl.id, can_store);
 			}
 			if(!can_store || stored_vessels.ContainsKey(vsl.id)) return;
+			//switch to hangar vessel before storing
+			if(FlightGlobals.ActiveVessel != vessel)
+				FlightGlobals.ForceSetActiveVessel(vessel);
 			//dock the vessel
 			StoredVessel stored_vessel = new StoredVessel();
 			//get vessel crew on board
@@ -303,11 +373,11 @@ namespace AtHangar
 			//store vessel
 			stored_vessel.vessel = vsl.BackupVessel();
 			stored_vessels.Add(vsl.id, stored_vessel);
-			used_volume += VesselVolume(vsl);
+			used_volume += Metric.Volume(vsl);
 			vessels_mass += vsl.GetTotalMass();
 			SetMass();
 			//destroy vessel
-			vsl.Die ();
+			vsl.Die();
 			FlightScreenMessager.showMessage("Vessel has been docked inside the hangar", 3);
 			Close();
 		}
@@ -318,7 +388,12 @@ namespace AtHangar
 			//if hangar is not ready, return
 			if(hangar_state == HangarState.Busy) 
 			{
-				FlightScreenMessager.showMessage("Prepare hangar first.", 3);
+				FlightScreenMessager.showMessage("Prepare hangar first", 3);
+				return;
+			}
+			if(hangar_gates.GatesState != HangarGates.Opened) 
+			{
+				FlightScreenMessager.showMessage("Open hangar gates first", 3);
 				return;
 			}
 			//if in orbit or on the ground and not moving
@@ -326,27 +401,27 @@ namespace AtHangar
 			{
 			case ClearToSaveStatus.NOT_IN_ATMOSPHERE:
 			{
-				FlightScreenMessager.showMessage("Cannot launch vessel while flying in atmosphere.", 3);
+				FlightScreenMessager.showMessage("Cannot launch vessel while flying in atmosphere", 3);
 				return;
 			}
 			case ClearToSaveStatus.NOT_UNDER_ACCELERATION:
 			{
-				FlightScreenMessager.showMessage("Cannot launch vessel hangar is under accelleration.", 3);
+				FlightScreenMessager.showMessage("Cannot launch vessel hangar is under accelleration", 3);
 				return;
 			}
 			case ClearToSaveStatus.NOT_WHILE_ABOUT_TO_CRASH:
 			{
-				FlightScreenMessager.showMessage("Cannot launch vessel while about to crush.", 3);
+				FlightScreenMessager.showMessage("Cannot launch vessel while about to crush", 3);
 				return;
 			}
 			case ClearToSaveStatus.NOT_WHILE_MOVING_OVER_SURFACE:
 			{
-				FlightScreenMessager.showMessage("Cannot launch vessel while moving on the surface.", 3);
+				FlightScreenMessager.showMessage("Cannot launch vessel while moving on the surface", 3);
 				return;
 			}
 			case ClearToSaveStatus.NOT_WHILE_THROTTLED_UP:
 			{
-				FlightScreenMessager.showMessage("Cannot launch vessel while thottled up.", 3);
+				FlightScreenMessager.showMessage("Cannot launch vessel while thottled up", 3);
 				return;
 			}
 			default:
@@ -368,6 +443,8 @@ namespace AtHangar
 			stored_vessel.vessel.Load(FlightDriver.FlightStateCache.flightState);
 			//get restored vessel from the world
 			Vessel vsl = FlightGlobals.Vessels[FlightGlobals.Vessels.Count-1];
+			vsl.Splashed = vessel.Splashed;
+			vsl.Landed = vessel.Landed;
 			//transfer crew back to the launched vessel
 			List<ProtoCrewMember> transfered_crew = new List<ProtoCrewMember>();
 			foreach(Part p in vessel.parts)	transfered_crew.AddRange(delCrew(p, stored_vessel.crew));
@@ -380,22 +457,28 @@ namespace AtHangar
 			}
 			else
 			{
-				used_volume -= VesselVolume(vsl);
+				used_volume -= Metric.Volume(vsl);
 				vessels_mass -= vsl.GetTotalMass();
 			}
 			SetMass();
 			//switch to restored vessel
 			FlightGlobals.ForceSetActiveVessel(vsl);
 			Staging.beginFlight();
+			FlightGlobals.overrideOrbit = true;
+			StartCoroutine(captureVessel(vsl, Vector3.zero));
+			Debug.Log(string.Format("[Hangar] actual new rotation: {0}", vsl.vesselTransform.rotation.eulerAngles));
+			Debug.Log(string.Format("[Hangar] actual new position: {0}", vsl.vesselTransform.position));
 			Open();
 		}
 		
 		//called every frame while part collider is touching the trigger
 		public void OnTriggerStay (Collider col) //see Unity docs
 		{
+			if(col == null) return;
 			if (hangar_gates.GatesState != HangarGates.Opened
 				|| !col.CompareTag ("Untagged")
-				|| col.gameObject.name == "MapOverlay collider") // kethane
+				|| col.gameObject.name == "MapOverlay collider"// kethane
+			    || col.attachedRigidbody == null)
 				return;
 			//get part and try to store vessel
 			Part p = col.attachedRigidbody.GetComponent<Part>();
@@ -406,8 +489,11 @@ namespace AtHangar
 		//called when part collider exits the trigger
 		public void OnTriggerExit(Collider col)
 		{
+			if (!col.CompareTag("Untagged")
+				|| col.gameObject.name == "MapOverlay collider" // kethane
+				|| col.attachedRigidbody == null)
+				return;
 			Part p = col.attachedRigidbody.GetComponent<Part>();
-			if (p == null) return;
 			if(probed_ids.ContainsKey(p.vessel.id)) probed_ids.Remove(p.vessel.id);
 		}
 		
---
+++ Source/Metric.cs
@@ -0,0 +1,166 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace AtHangar
+{
+	public class Metric
+	{
+		public float volume { get; private set; }
+		public Bounds bounds { get; private set; }
+		public Vector3 size { get { return bounds.size; } }
+		public int CrewCapacity { get; private set; }
+		
+		
+		private static Vector3[] bound_edges(Bounds b)
+		{
+			Vector3[] edges = new Vector3[8];
+			Vector3 min = b.min;
+			Vector3 max = b.max;
+			edges[0] = new Vector3(min.x, min.y, min.z);
+		    edges[1] = new Vector3(min.x, min.y, max.z);
+		    edges[2] = new Vector3(min.x, max.y, min.z);
+		    edges[3] = new Vector3(min.x, max.y, max.z);
+		    edges[4] = new Vector3(max.x, min.y, min.z);
+		    edges[5] = new Vector3(max.x, min.y, max.z);
+		    edges[6] = new Vector3(max.x, max.y, min.z);
+		    edges[7] = new Vector3(max.x, max.y, max.z);
+			return edges;
+		}
+		
+		private static void local2local(Transform _from, Transform _to, Vector3[] points)
+		{
+			for(int p=0; p < points.Length; p++)
+				points[p] = _to.InverseTransformPoint(_from.TransformPoint(points[p]));
+		}
+		
+		private static float boundsVolume(Bounds b)
+		{ return b.size.x*b.size.y*b.size.z; }
+		
+		private Bounds partsBounds(List<Part> parts, Transform vT)
+		{
+			Bounds b = new Bounds();
+			foreach(Part p in parts)
+			{
+				foreach(MeshFilter m in p.FindModelComponents<MeshFilter>())
+				{
+					if(m.renderer == null) continue;
+//					Debug.Log(string.Format("mesh: {0}", m.name));
+					Transform mT = m.transform;
+					Vector3[] edges = bound_edges(m.sharedMesh.bounds);
+					local2local(mT, vT, edges);
+					foreach(Vector3 edge in edges)
+						b.Encapsulate(edge);
+				}
+				CrewCapacity += p.CrewCapacity;
+			}
+			return b;
+		}
+		
+		
+		//empty metric
+		public Metric()
+		{
+			bounds = new Bounds();
+			volume = 0f;
+			CrewCapacity = 0;
+		}
+		
+		//metric copy
+		public Metric(Metric m)
+		{
+			bounds = new Bounds(m.bounds.center, m.bounds.size);
+			volume = m.volume;
+			CrewCapacity = m.CrewCapacity;
+		}
+		
+		//mesh metric
+		public Metric(Part part, string mesh_name)
+		{
+			MeshFilter m = part.FindModelComponent<MeshFilter>(mesh_name);
+			if(m == null) { Debug.LogWarning(string.Format("{0} does not have '{1}' mesh", part.name, m.name)); return; }
+			Bounds b = new Bounds();
+			Transform pT = part.partTransform;
+			Transform mT = m.transform;
+			Vector3[] edges = bound_edges(m.sharedMesh.bounds);
+			local2local(mT, pT, edges);
+			foreach(Vector3 edge in edges)
+				b.Encapsulate(edge);
+			bounds = b;
+			volume = boundsVolume(bounds);
+		}
+		
+		//part metric
+		public Metric(Part part)
+		{
+			Transform pT = part.partTransform;
+			bounds = partsBounds(new List<Part>(){part}, pT);
+			volume = boundsVolume(bounds);
+			CrewCapacity = part.CrewCapacity;
+		}
+		
+		//vessel metric
+		public Metric(Vessel vessel)
+		{
+			bounds = partsBounds(vessel.parts, vessel.vesselTransform);
+			volume = boundsVolume(bounds);
+		}
+		
+		//in-editor vessel metric
+		public Metric(List<Part> vessel)
+		{
+			Transform vT = vessel[0].partTransform;
+			bounds = partsBounds(vessel, vT);
+			volume = boundsVolume(bounds);
+		}
+		
+		//public methods
+		public bool empy() { return volume == 0; }
+		
+		public bool fits(Metric other)
+		{
+			List<float>  D = new List<float>{size.x, size.y, size.z};
+			List<float> _D = new List<float>{other.size.x, other.size.y, other.size.z};
+			D.Sort(); _D.Sort();
+			foreach(float d in D)
+			{
+				float ud = -1;
+				foreach(float _d in _D)
+				{
+					if(d <= _d)
+					{
+						ud = _d;
+						break;
+					}
+				}
+				if(ud < 0) return false;
+				_D.Remove(ud);
+			}
+			return true;
+		}
+		
+		public void scale(float s)
+		{
+			Bounds b = bounds;
+			b.SetMinMax(b.center-b.extents*s, b.center+b.extents*s);
+			bounds = b;
+			volume = boundsVolume(bounds);
+		}
+		
+		public static Metric operator*(Metric m, float scale)
+		{
+			Metric _new = new Metric(m);
+			_new.scale(scale);
+			return _new;
+		}
+		
+		public static Metric operator/(Metric m, float scale)
+		{ return m*(1.0f/scale); }
+		
+		public static float Volume(Part part) { return (new Metric(part)).volume; }
+		public static float Volume(Vessel vessel) { return (new Metric(vessel)).volume; }
+	}
+}
+
