55eca760899a8d48bc282d47995b1c185cc7a168
Allis Tauri <allista@gmail.com>
2014-06-19 12:39:35 +0400

    Added separate length resizeing.

--- Source/HangarPartResizer.cs
+++ Source/HangarPartResizer.cs
@@ -14,12 +14,17 @@ namespace AtHangar
 		[UI_FloatEdit(scene=UI_Scene.Editor, minValue=0.1f, maxValue=10, incrementLarge=1.25f, incrementSmall=0.125f, incrementSlide=0.001f)]
 		public float size = 1.0f;
 		
-//		[KSPField(isPersistant=true, guiActiveEditor=true, guiName="Length", guiFormat="S4")]
-//		[UI_FloatEdit(scene=UI_Scene.Editor, minValue=0.1f, maxValue=10, incrementLarge=1.25f, incrementSmall=0.125f, incrementSlide=0.001f)]
-//		public float length = 1.0f;
+		[KSPField(isPersistant=true, guiActiveEditor=true, guiName="Length", guiFormat="S4")]
+		[UI_FloatEdit(scene=UI_Scene.Editor, minValue=0.1f, maxValue=10, incrementLarge=1.0f, incrementSmall=0.1f, incrementSlide=0.001f)]
+		public float length = 1.0f;
+		
+		[KSPField] public bool lengthOnly = false;
 		
 		[KSPField] public float sizeStepLarge = 1.25f;
 		[KSPField] public float sizeStepSmall = 0.125f;
+		
+		[KSPField] public float lengthStepLarge = 1.0f;
+		[KSPField] public float lengthStepSmall = 0.1f;
 		
 		[KSPField] public Vector4 specificMass = new Vector4(0.005f, 0.011f, 0.009f, 0f);
 		[KSPField] public float specificBreakingForce  = 1536;
@@ -32,70 +37,81 @@ namespace AtHangar
 		public string massDisplay;
 		
 		protected float old_size   = -1000;
+		protected float old_length = -1000;
 		protected bool just_loaded = false;
 		
+		
+		private Vector3 scale_vector(Vector3 v, float s, float l)
+		{ return Vector3.Scale(v, new Vector3(s, s*l, s)); }
 		
 		public override void OnStart (StartState state)
 		{
 			base.OnStart (state);
-			
-			if (HighLogic.LoadedSceneIsEditor) {
+			if (HighLogic.LoadedSceneIsEditor) 
+			{
 				float minSize = Utils.getTechMinValue (minSizeName, 0.1f);
 				float maxSize = Utils.getTechMaxValue (maxSizeName, 10);
-			
-				Utils.setFieldRange (Fields ["size"], minSize, maxSize);
-			
-				((UI_FloatEdit)Fields ["size"].uiControlEditor).incrementLarge = sizeStepLarge;
-				((UI_FloatEdit)Fields ["size"].uiControlEditor).incrementSmall = sizeStepSmall;
+				
+				if(!lengthOnly)
+				{
+					Utils.setFieldRange (Fields ["size"], minSize, maxSize);
+					((UI_FloatEdit)Fields ["size"].uiControlEditor).incrementLarge = sizeStepLarge;
+					((UI_FloatEdit)Fields ["size"].uiControlEditor).incrementSmall = sizeStepSmall;
+				}
+				else Fields["size"].guiActiveEditor=false;
+				
+				Utils.setFieldRange (Fields ["length"], minSize, maxSize);
+				((UI_FloatEdit)Fields ["length"].uiControlEditor).incrementLarge = lengthStepLarge;
+				((UI_FloatEdit)Fields ["length"].uiControlEditor).incrementSmall = lengthStepSmall;
 			}
-			
-			updateNodeSize (size);
+			updateNodeSize(size);
 		}
 		
 		public override void OnLoad (ConfigNode cfg)
 		{
-			base.OnLoad (cfg);
+			base.OnLoad(cfg);
 			just_loaded = true;
 			if (HighLogic.LoadedSceneIsEditor || HighLogic.LoadedSceneIsFlight)
-				updateNodeSize (size);
+				updateNodeSize(size);
 		}
 		
 		public virtual void FixedUpdate ()
 		{
-			if (size != old_size) resizePart (size);
+			if (size != old_size || length != old_length) 
+				resizePart(size, length);
 			just_loaded = false;
 		}
 		
-		public void scaleNode (AttachNode node, float scale, bool setSize)
+		public void scaleNode(AttachNode node, float scale, float len, bool setSize)
 		{
-			if (node == null)
-				return;
-			node.position = node.originalPosition * scale;
+			if(node == null) return;
+			node.position = scale_vector(node.originalPosition, scale, len);
 			if (!just_loaded)
 				Utils.updateAttachedPartPos (node, part);
 			if (setSize)
 				node.size = Mathf.RoundToInt (scale / sizeStepLarge);
 		}
 		
-		public void setNodeSize (AttachNode node, float scale)
+		public void setNodeSize(AttachNode node, float scale)
 		{
-			if (node == null)
-				return;
+			if (node == null) return;
 			node.size = Mathf.RoundToInt (scale / sizeStepLarge);
 		}
 		
 		public virtual void updateNodeSize (float scale)
 		{
-			setNodeSize (part.findAttachNode ("top"), scale);
-			setNodeSize (part.findAttachNode ("bottom"), scale);
+			setNodeSize(part.findAttachNode ("top"), scale);
+			setNodeSize(part.findAttachNode ("bottom"), scale);
 		}
 		
-		public virtual void resizePart (float scale)
+		public virtual void resizePart (float scale, float len)
 		{
-			old_size = size;
+			old_size   = size;
+			old_length = length;
 		
 			//change mass and forces
-			part.mass = ((specificMass.x * scale + specificMass.y) * scale + specificMass.z) * scale + specificMass.w;
+			part.mass  = ((specificMass.x * scale + specificMass.y) * scale + specificMass.z) * scale + specificMass.w;
+			part.mass *= len;
 			massDisplay = Utils.formatMass (part.mass);
 			part.breakingForce = specificBreakingForce * Mathf.Pow (scale, 2);
 			part.breakingTorque = specificBreakingTorque * Mathf.Pow (scale, 2);
@@ -103,7 +119,7 @@ namespace AtHangar
 			//change scale
 			var model = part.FindModelTransform ("model");
 			if (model != null)
-				model.localScale = Vector3.one * scale;
+				model.localScale = scale_vector(Vector3.one, scale, len);
 			else
 				Debug.LogError ("[HangarPartResizer] No 'model' transform in the part", this);
 			
@@ -111,8 +127,8 @@ namespace AtHangar
 			Hangar hangar = part.Modules.OfType<Hangar>().SingleOrDefault();
 			if(hangar != null) hangar.Setup();
 		
-			scaleNode (part.findAttachNode ("top"), scale, true);
-			scaleNode (part.findAttachNode ("bottom"), scale, true);
+			scaleNode (part.findAttachNode ("top"), scale, len, true);
+			scaleNode (part.findAttachNode ("bottom"), scale, len, true);
 		}
 	}
 }
