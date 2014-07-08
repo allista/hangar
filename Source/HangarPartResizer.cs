accf2a01a837e446202d737218ff6d34793e17ab
Allis Tauri <allista@gmail.com>
2014-07-08 10:10:07 +0400

    Changed resizing.
    Use aspect instead of length with separate min-max-step values. Size is
    not defined in absolute values (from 0.5 to 4, like all other parts)
    instead of relative scale values.

--- Source/HangarPartResizer.cs
+++ Source/HangarPartResizer.cs
@@ -10,39 +10,45 @@ namespace AtHangar
 {
 	public class HangarPartResizer : PartModule
 	{
-		[KSPField(isPersistant=true, guiActiveEditor=true, guiName="Scale", guiFormat="S4")]
-		[UI_FloatEdit(scene=UI_Scene.Editor, minValue=0.5f, maxValue=10, incrementLarge=1.25f, incrementSmall=0.125f, incrementSlide=0.001f)]
+		[KSPField(isPersistant=true, guiActiveEditor=true, guiName="Size", guiFormat="S4")]
+		[UI_FloatEdit(scene=UI_Scene.Editor, minValue=0.5f, maxValue=10, incrementLarge=1.0f, incrementSmall=0.1f, incrementSlide=0.001f)]
 		public float size = 1.0f;
 		
-		[KSPField(isPersistant=true, guiActiveEditor=true, guiName="Length", guiFormat="S4")]
+		[KSPField(isPersistant=true, guiActiveEditor=true, guiName="Aspect", guiFormat="S4")]
 		[UI_FloatEdit(scene=UI_Scene.Editor, minValue=0.5f, maxValue=10, incrementLarge=1.0f, incrementSmall=0.1f, incrementSlide=0.001f)]
-		public float length = 1.0f;
+		public float aspect = 1.0f;
 		
-		[KSPField] public bool sizeOnly = false;
-		[KSPField] public bool lengthOnly = false;
-		[KSPField] public float offset = 1;
+		[KSPField(isPersistant=true)] private float orig_size = -1;
+		
+		[KSPField] public bool sizeOnly   = false;
+		[KSPField] public bool aspectOnly = false;
 		
 		[KSPField] public float minSize = 0.5f;
 		[KSPField] public float maxSize = 10f;
 		
+		[KSPField] public float minAspect = 0.5f;
+		[KSPField] public float maxAspect = 10f;
+		
 		[KSPField] public float sizeStepLarge = 1.0f;
 		[KSPField] public float sizeStepSmall = 0.1f;
 		
-		[KSPField] public float lengthStepLarge = 1.0f;
-		[KSPField] public float lengthStepSmall = 0.1f;
+		[KSPField] public float aspectStepLarge = 0.5f;
+		[KSPField] public float aspectStepSmall = 0.1f;
 		
 		[KSPField] public Vector4 specificMass = new Vector4(0.005f, 0.011f, 0.009f, 0f);
 		[KSPField] public float specificBreakingForce  = 1536;
 		[KSPField] public float specificBreakingTorque = 1536;
 		
-		[KSPField] public string minSizeName = "HANGAR_MINSCALE";
-		[KSPField] public string maxSizeName = "HANGAR_MAXSCALE";
+		[KSPField] public string minSizeName   = "HANGAR_MINSCALE";
+		[KSPField] public string maxSizeName   = "HANGAR_MAXSCALE";
+		[KSPField] public string minAspectName = "HANGAR_MINASPECT";
+		[KSPField] public string maxAspectName = "HANGAR_MAXASPECT";
 		
 		[KSPField(isPersistant=false, guiActiveEditor=true, guiName="Mass")]
 		public string massDisplay;
 		
 		private float old_size   = -1000;
-		private float old_length = -1000;
+		private float old_aspect = -1000;
 		private bool just_loaded = false;
 		
 		private Dictionary<string,int> orig_sizes = new Dictionary<string, int>();
@@ -57,34 +63,39 @@ namespace AtHangar
 			//check if the part has the hangar module
 			if (HighLogic.LoadedSceneIsEditor) 
 			{
-				//calculate min and max sizes from tech tree and module fields
-				float min_Size = Utils.getTechMinValue(minSizeName, 0.5f)/offset;
-				float max_Size = Utils.getTechMaxValue(maxSizeName, 10)/offset;
-				if(max_Size < 1) max_Size = 1;
+				//remember initial values
+				if(orig_size < 0) { orig_size = size; aspect = 1; }
+				//calculate min and max sizes from tech tree
+				float min_Size   = Utils.getTechMinValue(minSizeName, 0.5f);
+				float max_Size   = Utils.getTechMaxValue(maxSizeName, 10);
+				float min_Aspect = Utils.getTechMinValue(minAspectName, 0.5f);
+				float max_Aspect = Utils.getTechMaxValue(maxAspectName, 10);
 				//truncate min-max values at hard limits
 				if(minSize < min_Size) minSize = min_Size;
 				if(maxSize > max_Size) maxSize = max_Size;
+				if(minAspect < min_Aspect) minAspect = min_Aspect;
+				if(maxAspect > max_Aspect) maxAspect = max_Aspect;
 				//setup sliders
-				if(sizeOnly && lengthOnly) lengthOnly = false;
-				if(lengthOnly) Fields["size"].guiActiveEditor=false;
+				if(sizeOnly && aspectOnly) aspectOnly = false;
+				if(aspectOnly) Fields["size"].guiActiveEditor=false;
 				else
 				{
 					Utils.setFieldRange (Fields ["size"], minSize, maxSize);
 					((UI_FloatEdit)Fields ["size"].uiControlEditor).incrementLarge = sizeStepLarge;
 					((UI_FloatEdit)Fields ["size"].uiControlEditor).incrementSmall = sizeStepSmall;
 				}
-				if(sizeOnly) Fields["length"].guiActiveEditor=false;
+				if(sizeOnly) Fields["aspect"].guiActiveEditor=false;
 				else
 				{
-					Utils.setFieldRange (Fields ["length"], minSize, maxSize);
-					((UI_FloatEdit)Fields ["length"].uiControlEditor).incrementLarge = lengthStepLarge;
-					((UI_FloatEdit)Fields ["length"].uiControlEditor).incrementSmall = lengthStepSmall;
+					Utils.setFieldRange (Fields ["aspect"], minAspect, maxAspect);
+					((UI_FloatEdit)Fields ["aspect"].uiControlEditor).incrementLarge = aspectStepLarge;
+					((UI_FloatEdit)Fields ["aspect"].uiControlEditor).incrementSmall = aspectStepSmall;
 				}
 			}
 			//save original sizes of nodes
 			foreach(AttachNode node in part.attachNodes)
 				orig_sizes[node.id] = node.size;
-			updateNodeSizes(size);
+			updateNodeSizes();
 		}
 		
 		public override void OnLoad (ConfigNode cfg)
@@ -93,14 +104,13 @@ namespace AtHangar
 			just_loaded = true;
 		}
 		
-		public virtual void FixedUpdate ()
+		public void FixedUpdate ()
 		{
-			if (size != old_size || length != old_length) 
-				resizePart(size, length);
+			if (size != old_size || aspect != old_aspect) resizePart();
 			just_loaded = false;
 		}
 		
-		public virtual void updateDockingNode()
+		private void updateDockingNode()
 		{
 			ModuleDockingNode dock = part.Modules.OfType<ModuleDockingNode>().SingleOrDefault();
 			if(dock == null) return;
@@ -109,18 +119,18 @@ namespace AtHangar
 			dock.nodeType = string.Format("size{0}", node.size);
 		}
 		
-		public virtual void updateNodeSizes(float scale)
+		private void updateNodeSizes()
 		{
 			foreach(AttachNode node in part.attachNodes)
 			{
-				int new_size = orig_sizes[node.id] + Mathf.RoundToInt(scale/sizeStepLarge) - 1;
+				int new_size = orig_sizes[node.id] + Mathf.RoundToInt(size/orig_size/sizeStepLarge) - 1;
 				if(new_size < 0) new_size = 0;
 				node.size = new_size;
 			}
 			updateDockingNode();
 		}
 		
-		public void scaleNodes(float scale, float len)
+		private void scaleNodes(float scale, float len)
 		{
 			foreach(AttachNode node in part.attachNodes)
 			{
@@ -131,31 +141,29 @@ namespace AtHangar
 		}
 
 		
-		public virtual void resizePart(float scale, float len)
+		public void resizePart()
 		{
+			//calculate scale
+			float scale = size/orig_size; 
 			//change scale
 			Transform model = part.FindModelTransform ("model");
-			if(model != null) model.localScale = scale_vector(Vector3.one, scale, len);
+			if(model != null) model.localScale = scale_vector(Vector3.one, scale, aspect);
 			else Debug.LogError ("[HangarPartResizer] No 'model' transform in the part", this);
-			
 			//recalculate mass
-			part.mass   = ((specificMass.x * scale + specificMass.y * len) * scale + specificMass.z) * scale + specificMass.w;
+			part.mass   = ((specificMass.x * scale + specificMass.y * aspect) * scale + specificMass.z) * scale + specificMass.w;
 			massDisplay = Utils.formatMass (part.mass);
-			
 			//change breaking forces
 			part.breakingForce  = specificBreakingForce  * Mathf.Pow(scale, 2);
 			part.breakingTorque = specificBreakingTorque * Mathf.Pow(scale, 2);
-			
 			//change volume if the part is a hangar, and let the hangar module handle mass calculations
 			Hangar hangar = part.Modules.OfType<Hangar>().SingleOrDefault();
 			if(hangar != null) hangar.Setup();
-		
-			scaleNodes(scale, len);
-			updateNodeSizes(scale);
-			
+			//update nodes
+			scaleNodes(scale, aspect);
+			updateNodeSizes();
+			//save size and aspect
 			old_size   = size;
-			old_length = length;
+			old_aspect = aspect;
 		}
 	}
 }
-
