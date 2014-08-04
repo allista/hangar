60049d38d6224b394a07a0bc7981e5fdce0826a2
Allis Tauri <allista@gmail.com>
2014-08-04 16:26:55 +0400

    Fixed #6 by adding vessel rotations when packing.

--- Source/VesselsPack.cs
+++ Source/VesselsPack.cs
@@ -1,6 +1,7 @@
 //Packing algorithm based on <http://www.blackpawn.com/texts/lightmaps/default.html>  
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace AtHangar
@@ -24,32 +25,55 @@ namespace AtHangar
 		}
 		public Node(Metric space) : this(Vector3.zero, space.size) { }
 		public Node(Node n) : this(n.pos, n.size) {}
-		
+
+		//all possible rotations of "s" are considered
 		public static bool operator>(Node n, Vector3 s)
-		{ return n.size.x > s.x && n.size.y > s.y && n.size.z > s.z; }
-		
+		{ 
+			float[] nd = { n.size.x, n.size.y, n.size.z };
+			float[] sd = { s.x, s.y, s.z };
+			Array.Sort(nd); Array.Sort(sd);
+			return nd[0] > sd[0] && nd[1] > sd[1] && nd[2] > sd[2]; 
+		}
+
+		//all possible rotations of "s" are considered
 		public static bool operator<(Node n, Vector3 s)
-		{ return n.size.x < s.x || n.size.y < s.y || n.size.z < s.z; }
+		{ 
+			float[] nd = { n.size.x, n.size.y, n.size.z };
+			float[] sd = { s.x, s.y, s.z };
+			Array.Sort(nd); Array.Sort(sd);
+			return nd[0] < sd[0] || nd[1] < sd[1] || nd[2] < sd[2]; 
+		}
+
+		//all possible rotations of "s" are considered
+		public bool Matches(Vector3 s)
+		{ 
+			float[] nd = { size.x, size.y, size.z };
+			float[] sd = { s.x, s.y, s.z };
+			Array.Sort(nd); Array.Sort(sd);
+			return nd[0] == sd[0] && nd[1] == sd[1] && nd[2] == sd[2];
+		}
 		
 		public void Save(ConfigNode node, bool debug = true)
 		{
 			node.AddValue("pos", ConfigNode.WriteVector(pos));
 			node.AddValue("size", ConfigNode.WriteVector(size));
-			node.AddValue("vid", vid);
 			if(first != null)
 			{
 				ConfigNode f_node = node.AddNode("FIRST");
 				ConfigNode s_node = node.AddNode("SECOND");
 				first.Save(f_node, false); second.Save(s_node, false);
 			}
+			else node.AddValue("vid", vid);
 			if(debug) Debug.Log(node);
 		}
 	}
+
 
 	public abstract class PackedVessel 
 	{ 
 		public Metric metric; 
 		public Guid id; 
+		public Vector3 size { get { return metric.size; } }
 
 		public abstract void Save(ConfigNode node);
 		public abstract void Load(ConfigNode node);
@@ -62,40 +86,59 @@ namespace AtHangar
 		
 		public VesselsPack() {}
 		public VesselsPack(Metric space) { this.space = space; }
+
+		//Chooses the best rotation of a size vector "s" to store it inside the box with the size vector "box_size".
+		//The rotation is considered optimal if it gives the absolute maximum remainder in one of the dimensions,
+		//if in other dimensions the vector still fits.
+		static Vector3 optimal_rotation(Vector3 box_size, Vector3 s)
+		{
+			Vector3[] vs = {s, //all six permutations of coorditates
+							new Vector3(s.x, s.z, s.y),
+							new Vector3(s.y, s.x, s.z),
+							new Vector3(s.y, s.z, s.x),
+							new Vector3(s.z, s.x, s.y),
+							new Vector3(s.z, s.y, s.x)};
+			Vector3 optimal = s; float delta = 0f;
+			foreach(Vector3 v in vs)
+			{
+				Vector3 d = box_size-v; //space remainder
+				if(d.x < 0 || d.y < 0 || d.z < 0) continue; //doesn't fit in one of the dimensions
+				float[] d_values = {d.x, d.y, d.z}; 
+				float max_d = d_values.Max();
+				if(delta < max_d) { delta = max_d; optimal = v; }
+			}
+			return optimal;
+		}
 		
-		bool add_vessel(Node n, V vsl)
+		bool add_vessel(Node n, Vector3 s, Guid id)
 		{
 			if(n.first != null)
-			{
-				if(add_vessel(n.first, vsl)) return true;
-				return add_vessel(n.second, vsl);
-			}
+				return add_vessel(n.first, s, id) || add_vessel(n.second, s, id);
 			else
 			{
-				Vector3 s = vsl.metric.size;
-				//if node is used, cannot store
+				//if leaf node is used or smaller, cannot store
 				if(n.vid != Guid.Empty || n < s) return false;
 				//if the vessel fits perfectly, store it
-				if(n.size == s)
+				if(n.Matches(s))
 				{
-					n.vid = vsl.id;
+					n.vid = id;
 					return true;
 				}
 				//clone the node
 				n.first  = new Node(n);
 				n.second = new Node(n);
-				//space leftovers
-				float dx = n.size.x - s.x;
-				float dy = n.size.y - s.y;
-				float dz = n.size.z - s.z;
+				//rotate the vessel if needed
+				s = optimal_rotation(n.size, s);
+				//space remainder
+				Vector3 d = n.size - s;
 				//partition node space
-				if(dx > dy && dx > dz)
+				if(d.x > d.y && d.x > d.z)
 				{
 					n.first.size.x   = s.x;
 					n.second.pos.x  += s.x;
 					n.second.size.x -= s.x;
 				}
-				else if(dy > dx && dy > dz)
+				else if(d.y > d.x && d.y > d.z)
 				{
 					n.first.size.y   = s.y;
 					n.second.pos.y  += s.y;
@@ -108,7 +151,7 @@ namespace AtHangar
 					n.second.size.z -= s.z;
 				}
 				//fit in the first subnode
-				return add_vessel(n.first, vsl);
+				return add_vessel(n.first, s, id);
 			}
 		}
 
@@ -119,7 +162,8 @@ namespace AtHangar
 		{
 			sort_vessels(vessels);
 			Node root = new Node(space);
-			foreach(V vsl in vessels) { if(!add_vessel(root, vsl)) return false; }
+			foreach(V vsl in vessels)
+			{ if(!add_vessel(root, vsl.size, vsl.id)) return false; }
 			return true;
 		}
 
@@ -128,7 +172,7 @@ namespace AtHangar
 			sort_vessels(vessels);
 			Node root = new Node(space);
 			List<V> rem = new List<V>();
-			foreach(V vsl in vessels) { if(!add_vessel(root, vsl)) rem.Add(vsl); }
+			foreach(V vsl in vessels) { if(!add_vessel(root, vsl.size, vsl.id)) rem.Add(vsl); }
 			return rem;
 		}
 		
