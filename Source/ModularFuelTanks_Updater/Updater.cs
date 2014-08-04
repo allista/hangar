2d5b10c5e9aa7437e4022c975114da6ce8a16526
Allis Tauri <allista@gmail.com>
2014-08-04 17:15:44 +0400

    Trying to add MFT support to PartResizer and some parts.

---
+++ Source/ModularFuelTanks_Updater/Updater.cs
@@ -0,0 +1,30 @@
+﻿using AtHangar;
+using RealFuels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ModularFuelTanks_Updater
+{
+	public class ModularFuelTanks_Updater : ModuleUpdater<ModuleFuelTanks>
+	{
+		public override void OnRescale(Scale scale)
+		{
+			module.ChangeVolume(base_module.volume * scale.absolute.cube);
+			if(!part.HasModule<ResourcesUpdater>()) return;
+			var mft_names = module.fuelList.Select(t => t.name);
+			var mft_resources = part.Resources.list.Where(r => mft_names.Contains(r.name));
+			foreach(PartResource resource in mft_resources)
+			{
+
+				resource.amount /= scale.relative.cube;
+				resource.maxAmount /= scale.relative.cube;
+			}
+//			foreach(PartResource r in part.Resources)
+//			{
+//				r.amount /= scale.relative.cube;
+//				r.maxAmount /= scale.relative.cube;
+//			}
+		}
+	}
+}
+
