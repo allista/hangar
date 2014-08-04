fb7a27fe3ae7df02720a3ba563fd5f71aabb89c5
Allis Tauri <allista@gmail.com>
2014-08-04 16:31:45 +0400

    Changed versions to v1.1.0. Compiled against KAE 1.7.0

--- Source/AssemblyInfo.cs
+++ Source/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
@@ -29,7 +29,7 @@ namespace AtHangar
 	public static class KSP_AVC_Info
 	{
 		public static readonly System.Version HangarVersion = Assembly.GetCallingAssembly().GetName().Version;
-		public static readonly Version MinKSPVersion = new Version(0,24,0);
+		public static readonly Version MinKSPVersion = new Version(0,24,2);
 		public static readonly Version MaxKSPVersion = new Version(0,24,2);
 		public static readonly string  VersionURL    = "https://raw.githubusercontent.com/allista/hangar/master/GameData/Hangar/Hangar.version";
 		public static readonly string  UpgradeURL    = "https://github.com/allista/hangar/releases";
