55fa83b8c5ae009542821c5d92f115ddaeadf9b7
Allis Tauri <allista@gmail.com>
2014-09-14 15:57:11 +0400

    Moved Triangle and Quad to Utils. Moved mesh generation to WriteTo method.
    Also added SurfaceArea, GetTangentalBasis and NewSurfacePosition methods
    to simplify calculations in ProceduralAdapter.

--- Source/TruncatedCone.cs
+++ Source/TruncatedCone.cs
@@ -1,128 +1,119 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace AtHangar
 {
-	class TruncatedCone
+	public class TruncatedCone
 	{
-		class Triangle : IEnumerable<int>
-		{
-			readonly protected int i1, i2, i3;
+		//cone properties
+		readonly public float R1, R2, H, Area;
 
-			public Triangle(int i1, int i2, int i3)
-			{ this.i1 = i1; this.i2 = i2; this.i3 = i3; }
+		//internal constants
+		float dR, h2, ny, nk;
 
-			public virtual IEnumerator<int> GetEnumerator()
-			{
-				yield return i1;
-				yield return i2;
-				yield return i3;
-			}
+		public static float SurfaceArea(float R1, float R2, float H)
+		{ return Mathf.PI*(R1*R1 + R2*R2 + (R1+R2)*Mathf.Sqrt(H*H + Mathf.Pow(R1-R2, 2))); }
 
-			IEnumerator IEnumerable.GetEnumerator()
-			{ return GetEnumerator(); }
+		public TruncatedCone(float R1, float R2, float H)
+		{ 
+			this.R1 = R1; this.R2 = R2; this.H = H; 
+			Area = SurfaceArea(R1, R2, H);
+			//constants
+			dR = R2-R1;
+			h2 = H/2f;
+			ny = -dR/Mathf.Sqrt(H*H+dR*dR);
+			nk = Mathf.Sqrt(1f - ny*ny);
 		}
 
-		class Quad : Triangle
-		{
-			readonly int i4;
-
-			public Quad(int i1, int i2, int i3, int i4)
-				: base(i1, i2, i3) { this.i4 = i4; }
-
-			public override IEnumerator<int> GetEnumerator ()
-			{
-				yield return i1;
-				yield return i2;
-				yield return i3;
-
-				yield return i3;
-				yield return i4;
-				yield return i1;
-			}
+		public Basis GetTangentalBasis(Vector3 pos) 
+		{ 
+			pos = new Vector3(pos.x, 0, pos.z).normalized;
+			Vector3 n = new Vector3(pos.x*nk, ny, pos.z*nk);
+			Vector3 f = new Vector3(pos.x*dR, H,  pos.z*dR).normalized;
+			Vector3 t = new Vector3(pos.z, 0, -pos.x).normalized;
+			return new Basis(t, n, f);
 		}
 
-		readonly int num_vertices;
-		readonly Vector3[] vertices;
-		readonly Vector3[] normals;
-		readonly Vector2[] uvs;
-		readonly Vector4[] tangents;
-		readonly List<Triangle> faces = new List<Triangle>();
+		public Vector3 NewSurfacePosition(Vector3 old_pos)
+		{
+			float R = Mathf.Sqrt(Mathf.Pow(dR/H*(old_pos.y+H/2) + R1, 2));
+			return new Vector3(old_pos.x, old_pos.y, old_pos.z).normalized*R;
+		}
 
-		public TruncatedCone(float R1, float R2, float H, int sides)
+		public void WriteTo(int sides, Mesh mesh, string name = null)
 		{
 			if(sides < 3) throw new InvalidProgramException("TruncatedCone: number of sides " +
 				"cannot be less than 3");
-			num_vertices = sides*4;
-			vertices = new Vector3[num_vertices];
-			normals  = new Vector3[num_vertices];
-			uvs      = new Vector2[num_vertices];
-			tangents = new Vector4[num_vertices];
+			//build the cone
+			int loop = sides+1;
+			int num_vertices = loop*4;
+			Vector3[] vertices = new Vector3[num_vertices];
+			Vector3[] normals  = new Vector3[num_vertices];
+			Vector2[] uvs      = new Vector2[num_vertices];
+			Vector4[] tangents = new Vector4[num_vertices];
 			//offsets
-			int bottom_base = sides*2;
-			int top_base    = sides*3;
+			int bottom_base = loop*2;
+			int top_base    = loop*3;
 			//constants
-			float dR = R1-R2;
-			float h2 = H/2f;
 			float fi = Mathf.PI*2f/sides;
 			float du = 1f/sides;
-			float ny = dR/Mathf.Sqrt(H*H+dR*dR);
-			float nk = Mathf.Sqrt(1f - ny*ny);
 			//top-bottom normals and tangents
-			Vector3 bn = new Vector3(0, -1, 0);
-			Vector4 bt = new Vector4(1,  0, 0, -1);
-			Vector3 tn = new Vector3(0,  1, 0);
-			Vector4 tt = new Vector4(1,  0, 0,  1);
+			Vector3 bn = new Vector3( 0, -1, 0);
+			Vector4 bt = new Vector4(-1,  0, 0, -1);
+			Vector3 tn = new Vector3( 0,  1, 0);
+			Vector4 tt = new Vector4( 1,  0, 0,  1);
 			//vertices
-			for(uint i = 0; i < sides; i++)
+			for(uint i = 0; i < loop; i++)
 			{
-				float a    = fi*i;
-				float x    = Mathf.Sin(a);
-				float z    = Mathf.Cos(a);
-				float u    = du*i;
-				Vector3 n  = new Vector3(x*nk, ny, z*nk);
-				Vector4 t  = new Vector4(z, 0, -x, 1);
+				float a   = fi*i;
+				float x   = Mathf.Sin(a);
+				float z   = Mathf.Cos(a);
+				float u   = du*i;
+				Vector3 n = new Vector3(x*nk, ny, z*nk);
+				Vector4 t = new Vector4(z, 0, -x, 1);
 				//bottom
 				vertices[i] = vertices[i+bottom_base] = new Vector3(R1*x, -h2, R1*z);
 				uvs[i] = new Vector2(u, 0);
 				normals[i]  = n;
 				tangents[i] = t;
-				uvs[i+bottom_base] = new Vector2(0.5f*x, 0.5f+0.5f*z);
+				uvs[i+bottom_base] = new Vector2(0.25f+0.25f*x, 0.75f+0.25f*z);
 				normals[i+bottom_base]  = bn;
 				tangents[i+bottom_base] = bt;
 				//top
-				vertices[sides+i] = vertices[i+top_base] = new Vector3(R2*x, h2, R2*z);
-				uvs[sides+i] = new Vector2(u, 0.49f); //leave a 1% gap between sides and bases
-				normals[sides+i]  = n;
-				tangents[sides+i] = t;
-				uvs[i+top_base] = new Vector2(0.5f+0.5f*x, 0.5f+0.5f*z);
+				vertices[loop+i] = vertices[i+top_base] = new Vector3(R2*x, h2, R2*z);
+				uvs[loop+i] = new Vector2(u, 0.49f); //leave a 1% gap between sides and bases
+				normals[loop+i]  = n;
+				tangents[loop+i] = t;
+				uvs[i+top_base] = new Vector2(0.75f+0.25f*x, 0.75f+0.25f*z);
 				normals[i+top_base]  = tn;
 				tangents[i+top_base] = tt;
 			}
+			//loop ends
+			vertices[loop-1]   = vertices[0];
+			vertices[loop*2-1] = vertices[loop];
+			//faces
+			List<Triangle> faces = new List<Triangle>();
 			//bases
 			for(int i = 2; i < sides; i++)
 			{
-				faces.Add(new Triangle(bottom_base, bottom_base+i-1, bottom_base+i));
+				faces.Add(new Triangle(bottom_base+i, bottom_base+i-1, bottom_base));
 				faces.Add(new Triangle(top_base, top_base+i-1, top_base+i));
 			}
 			//sides
-			for(int i = 1; i < sides; i++)
-				faces.Add(new Quad(i-1, sides+i-1, sides+i, i));
-		}
-
-		public void WriteTo(Mesh mesh, string name = null)
-		{
+			for(int i = 1; i < loop; i++)
+				faces.Add(new Quad(i, loop+i, loop-1+i, i-1));
+			//write to the mesh
 			List<int> triangles = new List<int>();
 			faces.ForEach(triangles.AddRange);
-			mesh.Clear ();
+			mesh.Clear();
 			if(name != null) mesh.name = name;
 			mesh.vertices  = vertices;
 			mesh.normals   = normals;
 			mesh.tangents  = tangents;
 			mesh.uv        = uvs;
 			mesh.triangles = triangles.ToArray();
+			mesh.RecalculateBounds();
 		}
 	}
 }
