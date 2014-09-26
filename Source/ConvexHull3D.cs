3c64906bbe52cf2c527dadd4843286376c140294
Allis Tauri <allista@gmail.com>
2014-09-26 20:03:52 +0400

    Working version of the ConvexHull. Slow though.

--- Source/ConvexHull3D.cs
+++ Source/ConvexHull3D.cs
@@ -6,13 +6,6 @@ using UnityEngine;
 
 namespace AtHangar
 {
-	public static class PlaneExtensions
-	{ 
-		public static void Flip(this Plane p) { p.normal = -p.normal; } 
-		public static void Orient(this Plane p, Vector3 c) 
-		{ if(p.GetSide(c)) p.normal = -p.normal; } 
-	}
-
 	public class Face : IEnumerable<Vector3>
 	{
 		public Plane   P  { get; private set; }
@@ -30,13 +23,20 @@ namespace AtHangar
 		IEnumerator IEnumerable.GetEnumerator()
 		{ return GetEnumerator(); }
 
-		public bool GetSide(Vector3 v) { return P.GetSide(v); }
+		public bool Visible(Vector3 p) { return P.GetDistanceToPoint(p) > 1e-6; }
 
-		public void Orient(Vector3 p) 
-		{ 
-			if(P.GetSide(p)) P.Flip();
+		public float DistanceTo(Vector3 p) { return P.GetDistanceToPoint(p); }
+
+		public void Flip()
+		{
+			Plane p = P;
+			p.normal = -p.normal; 
+			p.distance = -p.distance;
+			P = p;
 			var t = v0; v0 = v1; v1 = t;
 		}
+
+		public void Orient(Vector3 p) { if(Visible(p)) Flip(); }
 
 		public Face(Vector3 v0, Vector3 v1, Vector3 v2)
 		{
@@ -47,6 +47,18 @@ namespace AtHangar
 
 		public Face(Vector3 v0, Vector3 v1, Vector3 v2, Vector3 p)
 			: this(v0,v1,v2) { Orient(p); }
+
+		#if DEBUG
+		public void Log()
+		{
+			Utils.Log("Face:\n" +
+				"{0}\n" +
+				"{1}\n" +
+				"{2}\n" +
+				"n: {3}\n" +
+				"d: {4}", v0, v1, v2, P.normal, P.distance);
+		}
+		#endif
 	}
 
 	public class ConvexHull3D : IEnumerable<Vector3>
@@ -68,28 +80,70 @@ namespace AtHangar
 			return c;
 		}
 
+		public Vector3[] TranslatePoints(Vector3 move)
+		{
+			Vector3[] points = new Vector3[Points.Count];
+			for(int i = 0; i < Points.Count; i++) points[i] = Points[i]+move;
+			return points;
+		}
+
 		public ConvexHull3D(List<Vector3> points)
 		{
 			//initial checks
 			if(points.Count < 4) 
 				throw new NotSupportedException(string.Format("[Hangar] ConvexHull3D needs at least 4 edges, {0} given", points.Count));
-			if(points.Count == 4) //trivial case
+			//lists of faces
+			Points = new List<Vector3>();
+			Faces  = new List<Face>();
+			//build the seed tetrahedron
+			//:first face uses first three points
+			Faces.Add(new Face(points[0], points[1], points[2]));
+			//:now scan other points for the furthest from the f0
+			int furthest=3; float max_dist = 0f; Face f0 = Faces[0];
+			for(int i = 3; i < points.Count; i++)
 			{
-				Points = points;
+				float d = Math.Abs(f0.DistanceTo(points[i]));
+				if(d > max_dist)
+				{
+					max_dist = d;
+					furthest = i;
+				}
+			}
+			Vector3 fp = points[furthest];
+			f0.Orient(fp); //f0 is not visible now, 
+			//so its edges should be taken in the oposite direction
+			Faces.Add(new Face(fp, f0.v0, f0.v2));
+			Faces.Add(new Face(fp, f0.v2, f0.v1));
+			Faces.Add(new Face(fp, f0.v1, f0.v0));
+//			Vector3 c0 = Centroid(points[0], points[1], points[2], fp); //debug
+//			Utils.Log("Tetrahedron:\n{0}\n{1}\n{2}\n{3}\n" + //debug
+//				"Centroid: {4}\n" +
+//				"Centroid is inside: {5},\n" +
+//				"Distance from centroid to faces:\n" +
+//				"{6}, {7}, {8}, {9}", 
+//				points[0], points[1], points[2], points[furthest],
+//				c0,	Contains(c0),
+//				Faces[0].P.GetDistanceToPoint(c0), Faces[1].P.GetDistanceToPoint(c0),
+//				Faces[2].P.GetDistanceToPoint(c0), Faces[3].P.GetDistanceToPoint(c0));
+			//if this is a tetrahedron, all is done
+			if(points.Count == 4)
+			{
+				Points.AddRange(points);
 				return;
 			}
-			//lists of faces
-			Faces = new List<Face>();
-			//build the seed tetrahedron
-			var c0 = Centroid(points.GetRange(0,4).ToArray());
-			Faces.Add(new Face(points[0], points[1], points[2], c0));
-			Faces.Add(new Face(points[3], points[1], points[2], c0));
-			Faces.Add(new Face(points[3], points[2], points[3], c0));
-			Faces.Add(new Face(points[3], points[1], points[3], c0));
-			//incrementally udate the seed
-			Update(points.GetRange(4,points.Count-4));
+			//otherwise incrementally udate the seed
+			points.RemoveAt(furthest);
+			points.RemoveRange(0,3);
+			Update(points);
 		}
 		public ConvexHull3D(Vector3[] points) : this(new List<Vector3>(points)) {}
+
+		public ConvexHull3D Scale(float s)
+		{
+			Vector3[] scaled = new Vector3[Points.Count];
+			for(int i = 0; i < Points.Count; i++) scaled[i] = Points[i]*s;
+			return new ConvexHull3D(scaled);
+		}
 
 		/// <summary>
 		/// Incrementally update the existing hull using provided points.
@@ -104,10 +158,12 @@ namespace AtHangar
 				//check visibility for each face
 				visible.Clear();
 				foreach(Face f in Faces) 
-				{ if(f.GetSide(p)) visible.Add(f); }
+				{ if(f.Visible(p)) visible.Add(f); }
+//				Utils.Log("Visible faces:"); //debug
+//				visible.ForEach(f => f.Log()); //debug
 				//if no face visible, v is inside the hull
 				if(visible.Count == 0) continue;
-				//otherwise delete visible from faces 
+				//otherwise delete visible from Faces 
 				visible.ForEach(f => Faces.Remove(f));
 				//if only 1 face is visible
 				if(visible.Count == 1)
@@ -116,6 +172,7 @@ namespace AtHangar
 					Faces.Add(new Face(p, f.v0, f.v1));
 					Faces.Add(new Face(p, f.v1, f.v2));
 					Faces.Add(new Face(p, f.v2, f.v0));
+//					Utils.Log("Faces: {0}", Faces.Count);
 					continue;
 				}
 				//otherwise add all possible faces 
@@ -126,19 +183,43 @@ namespace AtHangar
 					tmp.Add(new Face(p, f.v1, f.v2));
 					tmp.Add(new Face(p, f.v2, f.v0));
 				}
-				//and filter the ones that are inside the hull
-				for(int fi = 0; fi < tmp.Count-1; fi++)
+//				Utils.Log("Tmp faces:"); //debug
+//				tmp.ForEach(f => f.Log()); //debug
+				//and filter out the ones that are inside the hull
+				for(int fi = 0; fi < tmp.Count; fi++)
 				{
 					Face f = tmp[fi];
-					for(int fj = fi; fj < tmp.Count; fj++)
+					foreach(Face other in tmp)
 					{
-						if(f.GetSide(tmp[fj].c)) 
+						if(f == other) continue;
+						if(f.Visible(other.c))
 						{ f = null; break; }
 					}
 					if(f != null) Faces.Add(f);
+//					else { Utils.Log("Removing face:"); tmp[fi].Log(); }
 				}
+//				Utils.Log("Faces: {0}", Faces.Count);
 			}
-			Faces.ForEach(Points.AddRange);
+//			Utils.Log("Faces: {0}", Faces.Count);
+//			int nump = Points.Count; //debug
+			HashSet<Vector3> _Points = new HashSet<Vector3>();
+			Faces.ForEach(f => { _Points.Add(f.v0); _Points.Add(f.v1); _Points.Add(f.v2); });
+			Points.Clear(); Points.AddRange(_Points);
+//			Utils.Log("ConvexHull: {0} was, {1} now", nump, Points.Count);//debug
+		}
+
+		public bool Contains(Vector3 p)
+		{ return Faces.TrueForAll(f => !f.Visible(p)); }
+
+		public bool Contains(IEnumerable<Vector3> points)
+		{ 
+			bool contains = true;
+			foreach(Vector3 p in points) 
+			{
+				contains &= Contains(p);
+				if(!contains) break;
+			}
+			return contains;
 		}
 
 		public void Save(ConfigNode node)
@@ -147,19 +228,20 @@ namespace AtHangar
 				node.AddValue("point", ConfigNode.WriteVector(p));
 		}
 
-		public void Load(ConfigNode node)
+		public static ConvexHull3D Load(ConfigNode node)
 		{
-			Points.Clear();
+			List<Vector3> points = new List<Vector3>();
 			foreach(ConfigNode.Value v in node.values)
 			{
 				Vector3 p;
 				try 
 				{ 
 					p = ConfigNode.ParseVector3(v.value); 
-					Points.Add(p);
+					points.Add(p);
 				}
 				catch(Exception ex)	{ Debug.LogException(ex); }
 			}
+			return new ConvexHull3D(points);
 		}
 	}
 }
