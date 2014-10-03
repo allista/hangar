ba975cf5c0a95c0ce7571acb08c2c2499b4e523f
Allis Tauri <allista@gmail.com>
2014-10-03 09:05:26 +0400

    Reverted from using GetGroundHit to OnCollisionEnter, as the former does not provide quick way to get the collided Part.
    Added green highlighting of stiffened wheels for debugging.

--- Source/WheelUpdater.cs
+++ Source/WheelUpdater.cs
@@ -8,7 +8,6 @@ namespace AtHangar
 	{ 
 		public readonly WheelCollider Collider;
 		readonly float forwardStiffness, sidewaysStiffness;
-		public int LastID;
 
 		public WheelFrictionChanger(Wheel w)
 		{ 
@@ -35,25 +34,18 @@ namespace AtHangar
 		ModuleWheel module;
 		readonly HashSet<uint> trigger_objects = new HashSet<uint>();
 		readonly List<WheelFrictionChanger> saved_wheels = new List<WheelFrictionChanger>();
-		IEnumerator<YieldInstruction> collision_checker;
+		int last_id;
 
-		public override void OnAwake() 
-		{ 
-			collision_checker = check_collisions();
-			StartCoroutine(collision_checker);
-		}
-
-		void OnDestroy() 
-		{ 
-			RestoreWheels(); 
-			StopCoroutine(collision_checker);
-		}
+		void OnDestroy() { RestoreWheels(); }
 
 		public void StiffenWheels() 
-		{ saved_wheels.ForEach(w => w.SetStiffness(1, 1)); }
+		{ saved_wheels.ForEach(w => w.SetStiffness(1, 1)); 
+			part.SetHighlightColor(XKCDColors.LightSeaGreen);//debug
+			part.SetHighlight(true); }//debug
 
 		public void RestoreWheels()
-		{ saved_wheels.ForEach(w => w.RestoreWheel()); }
+		{ saved_wheels.ForEach(w => w.RestoreWheel()); 
+			part.SetHighlightDefault(); }//debug
 
 		public void RegisterTrigger(uint id) 
 		{ 
@@ -65,41 +57,26 @@ namespace AtHangar
 		bool setup()
 		{
 			if(module != null) return true;
-			module = part.Modules.OfType<ModuleWheel>().FirstOrDefault();
+			module = part.GetModule<ModuleWheel>();
 			if(module == null) return false;
 			module.wheels.ForEach(w => saved_wheels.Add(new WheelFrictionChanger(w)));
 			return true;
 		}
 
-		IEnumerator<YieldInstruction> check_collisions()
+		void OnCollisionEnter(Collision collision) 
 		{
-			WheelHit hit;
-			while(true)
-			{
-				yield return new WaitForSeconds(0.1f);
-				if(!setup()) continue;
-				foreach(WheelFrictionChanger wc in saved_wheels)
-				{
-					if(!wc.Collider.GetGroundHit(out hit)) continue;
-					Utils.Log("Detected ground hit of the wheel {0} with the game object {1}", wc.Collider.GetInstanceID(), hit.collider.gameObject.GetInstanceID());//debug
-					//check object id
-					var obj = hit.collider.gameObject;
-					int id = obj.GetInstanceID();
-					if(id == wc.LastID) continue;
-					wc.LastID = id;
-					//check part
-					Part other_part = obj.GetComponentInChildren<Part>();
-					if(other_part != null)
-					{
-						Utils.Log("Other_part: {0}, {1}", other_part.name, other_part.flightID);//debug
-						Utils.Log("Is trigger: {0}", trigger_objects.Contains(other_part.flightID));//debug
-					}
-					else Utils.Log("No part was found");
-					if(other_part != null && trigger_objects.Contains(other_part.flightID)) 
-						wc.SetStiffness(1, 1);
-					else wc.RestoreWheel();
-				}
-			}
+			if(!setup()) return;
+			//check object id
+			int id = collision.gameObject.GetInstanceID();
+			if(id == last_id) return;
+			last_id = id;
+			Utils.Log("Detected ground hit of the wheel {0} with the game object {1}, {2}", 
+				part.flightID, id, collision.gameObject.name);//debug
+			//check part
+			Part other_part = collision.gameObject.GetComponent<Part>();
+			if(other_part != null && trigger_objects.Contains(other_part.flightID)) 
+				StiffenWheels();
+			else RestoreWheels();
 		}
 	}
 }
