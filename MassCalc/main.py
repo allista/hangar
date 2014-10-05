633de54a5ea100c9abb81c07b75b29e14b467c48
Allis Tauri <allista@gmail.com>
2014-10-05 16:34:11 +0400

    Moved classes to base_classes.py and components.py, added custom_volume class; Added generator and reaction_wheel components.

--- MassCalc/main.py
+++ MassCalc/main.py
@@ -2,242 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-class material:
-    def __init__(self, density, cost):
-        self.density = density
-        self.cost    = cost
-#end class
+from base_classes import material, surface, volume, ship
+from components import battery, generator, reaction_wheel
 
-class surface:
-    _unit_h = 0.005
-    
-    def __init__(self, S, h, m):
-        self._S   = S
-        self.h    = h
-        self.m    = m
-    
-    def S(self, scale=1, length=1):
-        return self._S*scale**2*length    
-    
-    def mass(self, scale=1, length=1):
-        return self.S(scale, length)*self.h*self.m.density;
-    
-    def cost(self, scale=1, length=1):
-        return self.S(scale, length)*self.h/self._unit_h*self.m.cost;
-    
-    def __str__(self):
-        return '[%sm^2 x %sm], %st/m^3, %st, %sCr' % (self.S(), 
-                                                   self.h, self.m.density, 
-                                                   self.mass(), self.cost())
-#end class
-
-class volume:
-    def __init__(self, V, d, name, cost, surface=None, subvolumes=[]):
-        self._V    = V
-        self.d     = d
-        self.name  = name
-        self._cost = cost
-        self._surface = surface
-        self._subvolumes = subvolumes
-    #end def
-        
-    #volume and surface area
-    def V(self, scale=1, length=1):
-        vol = self.full_V(scale, length) - sum(sv.full_V(scale, length) for sv in self._subvolumes)
-        assert vol>=0, \
-        ("Combined volume of sub-volumes is greater than the volume of '%s'" % self.name)
-        return vol
-    #end def
-    
-    def full_V(self, scale=1, length=1):
-        return self._V*scale**3*length
-    
-    def S(self, scale=1, length=1):
-        if self._surface is None: return 0 
-        return self._surface.S(scale, length)
-    
-    def full_S(self, scale=1, length=1):
-        return (self.S(scale, length) +
-                sum(sv.full_S(scale, length) for sv in self._subvolumes))
-    
-    #cost
-    def V_cost(self, scale=1, length=1):
-        return self.V(scale, length)*self._cost
-    
-    def full_V_cost(self, scale=1, length=1):
-        return (self.V_cost(scale, length) + 
-                sum(sv.full_V_cost(scale, length) for sv in self._subvolumes))
-
-    def S_cost(self, scale=1, length=1):
-        if self._surface is None: return 0 
-        return self._surface.cost(scale, length)
-    
-    def full_S_cost(self, scale=1, length=1):
-        return (self.S_cost(scale, length) + 
-                sum(sv.full_S_cost(scale, length) for sv in self._subvolumes))
-        
-    def full_cost(self, scale=1, length=1): 
-        return self.full_V_cost(scale, length) + self.full_S_cost(scale, length)
-
-    #mass
-    def V_mass(self, scale=1, length=1):
-        return self.V(scale, length)*self.d;
-    
-    def full_V_mass(self, scale=1, length=1):
-        return (self.V_mass(scale, length) + 
-                sum(sv.full_V_mass(scale, length) for sv in self._subvolumes))
-        
-    def S_mass(self, scale=1, length=1):
-        if self._surface is None: return 0 
-        return self._surface.mass(scale, length)
-    
-    def full_S_mass(self, scale=1, length=1):
-        return (self.S_mass(scale, length) + 
-                sum(sv.full_S_mass(scale, length) for sv in self._subvolumes))
-        
-    def full_mass(self, scale=1, length=1): 
-        return self.full_V_mass(scale, length) + self.full_S_mass(scale, length)
-    
-    #representation
-    def __str__(self):
-        simple = self._surface is None and not self._subvolumes
-        s  = ''
-        s += '%s: %sm^3, %s%st, %sCr\n' % (self.name, self.full_V(),
-                                           '%st/m^3 ' % self.d if simple else '', 
-                                           self.full_mass(), self.full_cost())
-        if not simple:
-            if self._surface is not None: 
-                s += '   surface: %s\n' % self._surface
-            s += '   content: %sm^3, %st/m^3, %st, %sCr\n' % (self.V(), self.d, 
-                                                              self.V_mass(), self.V_cost())
-            if len(self._subvolumes) > 0:
-                s += '   '+''.join(str(sv).replace('\n', '\n   ') for sv in self._subvolumes)
-                s = s[:-3]
-        return s
-#end class
-
-class battery(volume):
-    #stock densities:
-    #(0.005/0.033, 0.01/0.083, 0.02/0.1, 0.05/0.3, 0.2/1.6) = [0.15151515, 0.12048193, 0.2, 0.16666667, 0.125]
-    #stock costs per volume:
-    #(80/0.033, 360/0.083, 550/0.1, 880/0.3, 4500/1.6) = [2424.2424, 4337.3494, 5500, 2933.3333, 2812.5]
-    #per mass:
-    #(80/0.005, 360/0.01, 550/0.02, 880/0.05, 4500/0.2) = [16000, 36000, 27500, 17600, 22500]
-    #per energy:
-    #(80/100, 360/200, 550/400, 880/1000, 4500/4000) = [0.8, 1.8, 1.375, 0.88, 1.125]
-    #stock energy density:
-    #(100/0.033, 200/0.083, 400/0.1, 1000/0.3, 4000/1.6) = [3030.303, 2409.6386, 4000, 3333.3333, 2500]
-    #batteries of RoverLander: 0.444m^3, 0.5t, 1000El.u
-    _density        = 0.05/0.444   #t/m^3
-    _energy_cost    = 0.88         #Cr/El.u
-    _energy_density = 1000.0/0.444 #El.u/m^3
-    
-    def __init__(self, V, energy=-1):
-        if V < 0 and energy < 0: 
-            raise ValueError("battery: either V or energy should be positive")
-        #compute energy, energy density and volume
-        if        V < 0: V = energy/self._energy_density
-        elif energy < 0: energy = V*self._energy_density
-        else: self._energy_density = energy/float(V)
-        self.energy = energy
-        #initialize volume
-        volume.__init__(self, V, self._density, 'batteries', self._energy_cost*energy/V)
-    #end def
-    
-    def __str__(self):
-        s  = volume.__str__(self)
-        s += '   energy: %.1f\n' % self.energy
-        return s
-#end class
-
-
-class ship:
-    _asymptote_slope     = 1.5
-    _asymptote_intercept = 1e5
-    _exponent_base       = 1.25
-    
-    def __init__(self, name, volumes, add_mass = 0, add_cost = 0, res_cost = 0):
-        self.name       = name
-        self._volumes   = volumes
-        self._add_mass  = add_mass
-        self._add_cost  = add_cost
-        self._res_cost  = res_cost
-        self._spec_mass = np.array([self.V_mass(), self.S_mass(), 0, self._add_mass])
-        self._init_mass = sum(self._spec_mass)
-        self._weights   = self._spec_mass/self._init_mass
-        self._spec_cost = np.array([sum(v.full_V_cost() for v in self._volumes), 
-                                    sum(v.full_S_cost() for v in self._volumes), 
-                                    0, self._add_cost])
-        self._cost      = sum(self._spec_cost)
-        self._cost_weights = self._spec_cost/self._cost
-        self._cost     += res_cost
-    #end def
-    
-    def mass(self, scale=1, length=1):
-        w = self._spec_mass
-        m = ((w[0]*scale + w[1])*scale + w[2])*scale*length
-        if len(w) > 3: m += w[3]
-        return m
-    #end def
-    
-    def cost(self, scale=1, length=1):
-        w = self._spec_cost
-        c = ((w[0]*scale + w[1])*scale + w[2])*scale*length
-        if len(w) > 3: c += w[3]
-        c += self._res_cost
-        return c
-    #end def
-    
-    @classmethod
-    def entry_cost(cls, c):
-        return c*cls._asymptote_slope + (1-cls._exponent_base**(-10*c/cls._asymptote_intercept))*cls._asymptote_intercept
-    #end def
-    
-    def true_mass(self, scale=1, length=1):
-        return self.V_mass(scale, length)+self.S_mass(scale, length)+self._add_mass
-    #end def
-    
-    def volume(self, scale=1, length=1):
-        return sum(v.full_V(scale, length) for v in self._volumes)
-    
-    def surface(self, scale=1, length=1):
-        return sum(v.full_S(scale, length) for v in self._volumes)
-    
-    def V_mass(self, scale=1, length=1):
-        return sum(v.full_V_mass(scale, length) for v in self._volumes)
-    
-    def S_mass(self, scale=1, length=1):
-        return sum(v.full_S_mass(scale, length) for v in self._volumes)
-    #end def
-
-    def __str__(self):
-        s  = '//'+hr(self.name, '=')
-        s += '//'+'\n//'.join(str(v).replace('\n', '\n//') for v in self._volumes)[:-2]
-        s += '//'+hr()
-        s += '//Total volume:    %.3f m^3, %.6f t\n' % (self.volume(), self.V_mass())
-        s += '//Total surface:   %.3f m^2, %.6f t\n' % (self.surface(), self.S_mass())
-        s += '//Additional mass: %.6f t\n' % self._add_mass
-        s += '//Additional cost: %.3f Cr\n' % self._add_cost
-        s += '//Resources cost:  %.3f Cr\n' % self._res_cost
-        s += 'entryCost = %.3f\n' % self.entry_cost(self._cost)
-        s += 'cost = %.3f\n' % self._cost
-        s += 'mass = %.6f\n' % self._init_mass
-        s += 'specificMass = %s //weights: [ %s ]\n' % (', '.join(str(m) for m in self._spec_mass), 
-                                                        ', '.join(str(w) for w in self._weights))
-        s += 'specificCost = %s //weights: [ %s ]\n' % (', '.join(str(m) for m in self._spec_cost), 
-                                                        ', '.join(str(w) for w in self._cost_weights))
-        return s
-    #end def
-#end class
-         
-
-def hr(text='', ch='-', width=80):
-    tl = len(text)
-    if width-tl-2 < 0: return text
-    ll = (width-tl-2)/2
-    rl = width-tl-2-ll
-    return '%s %s %s\n' % (ch*ll, text, ch*rl)
-#end def
 
 def format_data(x, ys, w=None):
     s = ''
@@ -292,8 +59,8 @@ if __name__ == '__main__':
                                      surface(960.55, 0.007, composits),
                                      [volume(46.92, 0.01, 'machinery room', 15,
                                              subvolumes=[battery(V=-1, energy=20000),
-                                                         volume(0.95, 0.2/0.21, 'reaction wheel', 2100/0.21),
-                                                         volume(1, 0.72, 'generator', 29700),
+                                                         reaction_wheel(0.95),
+                                                         generator(V=-1, energy=6.75),
                                                          volume(2, 0.0, 'monopropellent tank', 0,
                                                                 surface(11.04, 0.006, aluminium))]),
                                       volume(112.64*2, 0.168, 'cabins', 200), #density of the SpaceX Dragon vessel
@@ -311,7 +78,7 @@ if __name__ == '__main__':
                       volumes=[volume(9.536, 0.01, 'hull', 2,
                                       surface(92.1, 0.004, Al_Li),
                                       [volume(3.498, 0.12, 'machinery', 220,
-                                              subvolumes=[volume(0.17, 0.2/0.21, 'reaction wheel', 2100/0.21)]),
+                                              subvolumes=[reaction_wheel(0.17)]),
                                        volume(2.225, 0.29, 'base', 40)]),
                                volume(0.62*2+0.47*2+0.0138*4, 0.02, 'doors', 1,
                                       surface(14.19*2+13.45*2, 0.003, Al_Li),
@@ -343,13 +110,13 @@ if __name__ == '__main__':
                      add_mass=0.04, #probe core
                      add_cost=100 + 480) #Light + probe core
     
-    big       = ship('BigHangar', 
+    big       = ship('BigHangar',
                      volumes=[volume(527.4, 0.01, 'hull', 2,
                                      surface(1667.79, 0.01, composits),
                                      [volume(218.99, 0.183, 'cabins', 150,
-                                             subvolumes=[volume(38.15, 0.321, 'machinery', 120,
+                                             subvolumes=[volume(38.15, 0.331, 'machinery', 130,
                                                                 subvolumes=[battery(V=-1, energy=40000),
-                                                                            volume(1.14, 0.72, 'generator', 29700)])])]),
+                                                                            generator(V=-1, energy=10)])])]),
                               volume(17.89, 0.01,'doors', 2,
                                      surface(124.08, 0.01, composits)),
                               volume(4.34, 0.78,'clamp', 300),
