8b06b2e5d1de44dfb9f7f38c7aa736efcf20011d
Allis Tauri <allista@gmail.com>
2014-10-28 01:25:54 +0300

    Added HangarStorageDynamic subclass: volume may be added to it through a public interface.

---
+++ Source/HangarStorageDynamic.cs
@@ -0,0 +1,39 @@
+﻿namespace AtHangar
+{
+	public class HangarStorageDynamic : HangarStorage
+	{
+		[KSPField(isPersistant = true)] public float CurrentVolume;
+		[KSPField] public float UpdateVolumeThreshold = 0.1f; //m^3
+
+
+		protected override void early_setup(StartState state)
+		{
+			base.early_setup(state);
+			Fields["hangar_v"].guiActive = true;
+			Fields["hangar_d"].guiActive = true;
+		}
+
+		protected override void update_metrics()
+		{
+			PartMetric = new Metric(part);
+			HangarMetric = new Metric(CurrentVolume);
+		}
+
+		public void UpdateMetric()
+		{
+			HangarMetric = new Metric(CurrentVolume);
+			hangar_v = Utils.formatVolume(HangarMetric.volume);
+			hangar_d = Utils.formatDimensions(HangarMetric.size);
+			_used_volume = Utils.formatPercent(UsedVolumeFrac);
+		}
+
+		public void AddVolume(float dV) 
+		{
+			if(dV < 0) return;
+			CurrentVolume += dV;
+			if(CurrentVolume - HangarMetric.volume > UpdateVolumeThreshold)
+				UpdateMetric();
+		}
+	}
+}
+
