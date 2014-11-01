40cbf487fbd52b56ed397662e9a3101e3aba89ad
Allis Tauri <allista@gmail.com>
2014-11-01 14:53:40 +0300

    Added SwitchableTank system that allows to have resource tanks with resource type switchable in flight.

---
+++ Source/AsteroidHangars/HangarSwitchableTank.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using KSPAPIExtensions;
+using UnityEngine;
+
+namespace AtHangar
+{
+	/// <summary>
+	/// This is a different approach than in ModularFuelTanks, more suitable for "cargo" resources than fuels:
+	/// Such tank may contain only one type of resources, but this type may be switched in-flight, 
+	/// if the part has zero amount of the current resource.
+	/// </summary>
+	public class HangarSwitchableTank : PartModule
+	{
+		/// <summary>
+		/// The camel case components matching regexp.
+		/// From: http://stackoverflow.com/questions/155303/net-how-can-you-split-a-caps-delimited-string-into-an-array
+		/// </summary>
+		const string CamelCaseRegexp = "([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))";
+		/// <summary>
+		/// The type of the tank. Types are defined in separate config nodes.
+		/// </summary>
+		[KSPField] public string TankType;
+		/// <summary>
+		/// The volume of a tank. Is defined in a config or calculated from the part volume in editor.
+		/// Cannot be changed in flight.
+		/// </summary>
+		[KSPField(isPersistant = true)] public float  Volume = -1f;
+
+		[KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "Resource")]
+		[UI_ChooseOption]
+		public string CurrentResource = string.Empty;
+		PartResource current_resource;
+
+		public ConfigNode ModuleConfig;
+		SwitchableTankType tank_type;
+
+
+		public override string GetInfo()
+		{
+			var info = "";
+			if(!init_tank_type()) return info;
+			info += tank_type.Info;
+			init_tank_volume();
+			info += "Tank Volume: " + Utils.formatVolume(Volume/1000f);
+			return info;
+		}
+
+		static void setup_res_chooser(string[] names, string[] values, UI_Control control)
+		{
+			var current_res_edit = control as UI_ChooseOption;
+			if(current_res_edit == null) return;
+			current_res_edit.display = names;
+			current_res_edit.options = values;
+		}
+
+		public override void OnStart(StartState state)
+		{
+			base.OnStart(state);
+			if(!init_tank_type()) return;
+			init_tank_volume();
+			//initialize UI
+			if(tank_type.Resources.Count > 1)
+			{
+				var res_values = tank_type.SortedNames.ToArray();
+				var res_names  = tank_type.SortedNames.ConvertAll(s => Regex.Replace(s, CamelCaseRegexp, "$1 ")).ToArray();
+				setup_res_chooser(res_names, res_values, Fields["CurrentResource"].uiControlEditor);
+				setup_res_chooser(res_names, res_values, Fields["CurrentResource"].uiControlFlight);
+			}
+			else Fields["CurrentResource"].guiActive = Fields["CurrentResource"].guiActiveEditor = false;
+			//initialize resource
+			if(CurrentResource == string.Empty) CurrentResource = tank_type.DefaultResource.Name;
+			switch_resource();
+			StartCoroutine(slow_update());
+		}
+
+		void init_tank_volume()
+		{
+			if(Volume > 0) return;
+			Volume = Metric.Volume(part)*tank_type.UsefulVolumeRatio*1000f;
+		}
+
+		bool init_tank_type()
+		{
+			tank_type = null;
+			if(ModuleConfig.HasNode(SwitchableTankType.NODE_NAME))
+			{
+				tank_type = new SwitchableTankType();
+				tank_type.Load(ModuleConfig.GetNode(SwitchableTankType.NODE_NAME));
+				if(!tank_type.Valid)
+				{
+					ScreenMessager.showMessage(6, "Hangar: Configuration of \"{0}\" Tank Type in \"{1}\" is INVALID.\n", 
+						tank_type.Name, this.Title());
+					tank_type = null;
+				}
+			}
+			else if(!SwitchableTankType.TankTypes.TryGetValue(TankType, out tank_type))
+				ScreenMessager.showMessage(6, "Hangar: No \"{0}\" tank type in the library. Configuration of \"{1}\" is INVALID.", 
+					TankType, this.Title());
+			enabled = isEnabled = tank_type != null;
+			return enabled;
+		}
+
+		public override void OnLoad(ConfigNode node)
+		{
+			base.OnLoad(node);
+			ModuleConfig = node;
+		}
+
+		public override void OnSave(ConfigNode node)
+		{
+			base.OnSave(node);
+			if(tank_type != null && tank_type.Valid)
+			{
+				ConfigNode tank_node = node.AddNode(SwitchableTankType.NODE_NAME);
+				tank_type.Save(tank_node);
+			}
+		}
+
+		bool switch_resource()
+		{
+			var res = tank_type[CurrentResource];
+			//if some resource is currently managed, check it's amount 
+			//and if it's zero, remove the resource
+			if(current_resource != null)
+			{
+				if(current_resource.amount > 0) 
+				{ 
+					ScreenMessager.showMessage("Cannot change resource type while tank is in use");
+					CurrentResource = current_resource.resourceName;
+					return false;
+				}
+				part.Resources.list.Remove(current_resource); 
+				Destroy(current_resource);
+				current_resource = null;
+			}
+			//calculate maxAmount
+			var maxAmount = Volume*res.UnitsPerLiter;
+			//if there is such resource already, just plug it in
+			var part_res = part.Resources[res.Name];
+			if(part_res != null)
+			{
+				current_resource = part_res;
+				current_resource.maxAmount = maxAmount;
+				if(current_resource.amount > current_resource.maxAmount)
+					current_resource.amount = current_resource.maxAmount;
+				return true;
+			}
+			//otherwise, create the new resource
+			var node = new ConfigNode("RESOURCE");
+			node.AddValue("name", res.Name);
+			node.AddValue("amount", 0);
+			node.AddValue("maxAmount", Volume*res.UnitsPerLiter);
+			part.Resources.Add(node);
+			current_resource = part.Resources[res.Name];
+			return true;
+		}
+
+		IEnumerator<YieldInstruction> slow_update()
+		{
+			while(true)
+			{
+				if(current_resource != null && 
+					current_resource.resourceName != CurrentResource)
+					switch_resource();
+				yield return new WaitForSeconds(0.1f);
+			}
+		}
+	}
+}
+
---
+++ Source/AsteroidHangars/SwitchableTankType.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AtHangar
+{
+	/// <summary>
+	/// Type of a switchable tank. 
+	/// Defines resources that a tank of this type can hold, 
+	/// how much units of each resource a liter of volume contains,
+	/// and portion of part's volume that can be used by the tank.
+	/// </summary>
+	public class SwitchableTankType : ConfigNodeObject
+	{
+		/// <summary>
+		/// The library of preconfigured tank types 
+		/// that is loaded on game start by HangarConfigLoader.
+		/// </summary>
+		public static Dictionary<string, SwitchableTankType> TankTypes 
+		{ 
+			get
+			{
+				if(_tank_types == null)
+				{
+					_tank_types = new Dictionary<string, SwitchableTankType>();
+					foreach(ConfigNode n in GameDatabase.Instance.GetConfigNodes(SwitchableTankType.NODE_NAME))
+					{
+						var tank_type = new SwitchableTankType();
+						tank_type.Load(n);
+						if(!tank_type.Valid)
+						{
+							ScreenMessager.showMessage(6, "Hangar: configuration of \"{0}\" tank type is INVALID.", tank_type.Name);
+							continue;
+						}
+						try { _tank_types.Add(tank_type.Name, tank_type); }
+						catch { Utils.Log("SwitchableTankType: ignoring duplicate configuration of {0} tank type", tank_type.Name); }
+					}
+				}
+				return _tank_types;
+			}
+		}
+		static Dictionary<string, SwitchableTankType> _tank_types;
+
+		new public const string NODE_NAME = "TANKTYPE";
+		/// <summary>
+		/// The name of the tank type.
+		/// </summary>
+		[Persistent] public string Name;
+		/// <summary>
+		/// The string list of resources a tank of this type can hold. Format:
+		/// ResourceName1 units_per_liter; ResourceName2 units_per_liter2; ...
+		/// </summary>
+		[Persistent] public string PossibleResources;
+		/// <summary>
+		/// The portion of a part's volume the tank can use.
+		/// </summary>
+		[Persistent] public float  UsefulVolumeRatio = 0.8f;
+
+		public Dictionary<string,TankResource> Resources { get; private set; }
+		public bool Valid { get { return Resources != null && Resources.Count > 0; } }
+		public List<string> SortedNames { get; private set; }
+		public TankResource DefaultResource { get; private set; }
+
+		public TankResource this[string name]
+		{
+			get
+			{
+				TankResource res = default(TankResource);
+				if(Valid) Resources.TryGetValue(name, out res);
+				return res;
+			}
+		}
+
+		public override void Load(ConfigNode node)
+		{
+			base.Load(node);
+			Resources = TankResource.ParseResourceList(PossibleResources);
+			if(Valid) 
+			{
+				var names = new List<string>(Resources.Keys); names.Sort();
+				SortedNames = names;
+				DefaultResource = Resources[SortedNames[0]];
+			}
+		}
+
+		public string Info
+		{ 
+			get 
+			{ 
+				var info = "";
+				if(!Valid) return info;
+				info += "Tank can hold:\n";
+				foreach(var r in SortedNames)
+					info += string.Format("- {0}: {1}u/L\n", Resources[r].Name, Resources[r].UnitsPerLiter);
+				return info;
+			} 
+		}
+	}
+
+
+	/// <summary>
+	/// A Part Resource Definition complemented with Units Per Liter ratio.
+	/// </summary>
+	public struct TankResource
+	{
+		public PartResourceDefinition Resource { get; private set; }
+		public float UnitsPerLiter { get; private set; }
+		public string Name { get { return Resource.name; } }
+		public bool Valid { get { return Resource != null; } }
+
+		public TankResource(PartResourceDefinition resource, float units_per_liter = 1f) : this()
+		{
+			Resource = resource;
+			UnitsPerLiter = units_per_liter;
+		}
+
+		public TankResource(string resource_definition) : this()
+		{
+			var name_and_value = resource_definition.Split(new []{' '}, 
+				StringSplitOptions.RemoveEmptyEntries);
+			if(name_and_value.Length != 2) 
+			{
+				Utils.Log("TankResource: Invalid format of tank resource definition. " +
+					"Should be 'ResourceName units_per_liter', got {0}", resource_definition);
+				return;
+			}
+			float val;
+			if(!float.TryParse(name_and_value[1], out val) || val <= 0)
+			{
+				Utils.Log("TankResource: Invalid format of units_per_liter. " +
+					"Should be positive float value, got: {0}", name_and_value[1]);
+				return;
+			}
+			var res_def = PartResourceLibrary.Instance.GetDefinition(name_and_value[0]);
+			if(res_def == null) 
+			{
+				Utils.Log("TankResource: Resource does not exist: {0}", name_and_value[0]);
+				return;
+			}
+			Resource = res_def;
+			UnitsPerLiter = val;
+		}
+
+		public static Dictionary<string, TankResource> ParseResourceList(string resources)
+		{
+			var res_dict = new Dictionary<string, TankResource>();
+			foreach(var _res_str in resources.Split(';'))
+			{
+				var res_str = _res_str.Trim();
+				if(res_str == string.Empty) continue;
+				var res = new TankResource(res_str);
+				if(!res.Valid) return null;
+				try { res_dict.Add(res.Name, res); }
+				catch(ArgumentException) {}
+			}
+			return res_dict;
+		}
+	}
+}
+
