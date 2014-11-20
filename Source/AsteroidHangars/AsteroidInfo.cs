770b4c1ccd536984cb35ad62af2dc72ed9bbf023
Allis Tauri <allista@gmail.com>
2014-11-20 09:02:19 +0300

    Added locking of asteroids from USI-ART to AsteroidInfo. Forcing fuelCrossFeed=false OnStart.

--- Source/AsteroidHangars/AsteroidInfo.cs
+++ Source/AsteroidHangars/AsteroidInfo.cs
@@ -5,11 +5,13 @@ namespace AtHangar
 {
 	public class AsteroidInfo : PartModule
 	{
+		const string USI_PotatoInfoName = "USI_PotatoInfo";
 		const float min_mass_ratio = 0.3f;
 
 		[KSPField(isPersistant = true)] public float OrigMass = -1f;
 		[KSPField(isPersistant = true)] public float MinMass  = -1f;
 		[KSPField(isPersistant = true)] public float Density  = -1f;
+		[KSPField(isPersistant = true)] public bool  Locked;
 
 		ModuleAsteroid asteroid;
 		float orig_crash_tolerance;
@@ -34,11 +36,34 @@ namespace AtHangar
 			}
 		}
 
+		public void LockAsteroid()
+		{
+			Locked = true;
+			try { part.Modules.Remove(part.Modules[USI_PotatoInfoName]); }
+			catch {}
+		}
+
+		public bool AsteroidIsUsable
+		{
+			get
+			{
+				try
+				{
+					var USI_PotatoInfo = part.Modules[USI_PotatoInfoName];
+					var explored = USI_PotatoInfo.Fields["Explored"];
+					return !(bool)explored.host;
+				} catch {}
+				return true;
+			}
+		}
+
 		public override void OnStart(StartState state)
 		{
 			base.OnStart(state);
+			part.fuelCrossFeed = false; //fuel? cross feed? through an asteroid?! >_<
 			orig_crash_tolerance = part.crashTolerance;
 			asteroid = part.GetModule<ModuleAsteroid>();
+			if(Locked) LockAsteroid();
 			StartCoroutine(init_params());
 		}
 	}
