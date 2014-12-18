1203a5c79625edd055a0cdc98a35d12cfcd35477
Allis Tauri <allista@gmail.com>
2014-12-19 02:03:16 +0300

    Added HangarFilterManager that creates custom part category. Added category icons. Closed #140.

--- Source/Utils/Addons.cs
+++ Source/Utils/Addons.cs
@@ -96,5 +96,59 @@ namespace AtHangar
 			}
 		}
 	}
+
+	/// <summary>
+	/// Addon that assembles a custom part filter by function in the part library.
+	/// The code is adapted from the RealChute mod by Christophe Savard (stupid_chris):
+	/// https://github.com/StupidChris/RealChute/blob/master/RealChute/RCFilterManager.cs
+	/// Many thanks to Chris for figuring this out so fast!
+	/// </summary>
+	[KSPAddon(KSPAddon.Startup.MainMenu, true)]
+	public class HangarFilterManager : MonoBehaviour
+	{
+		const string CATEGORY = "Hangars";
+		const string ICON = "HangarCategory";
+
+		void Awake()
+		{ GameEvents.onGUIEditorToolbarReady.Add(add_filter); }
+
+		static bool has_hangar(string module_name)
+		{
+			if(module_name == typeof(Hangar).Name) return true;
+			if(module_name == typeof(HangarGateway).Name) return true;
+			if(module_name == typeof(HangarStorage).Name) return true;
+			if(module_name == typeof(SimpleHangarStorage).Name) return true;
+			if(module_name == typeof(SingleUseHangarStorage).Name) return true;
+			return false;
+		}
+
+		void add_filter()
+		{
+			//load the icon
+			if(!PartCategorizer.Instance.iconDictionary.ContainsKey(ICON))
+			{
+				var _icon   = GameDatabase.Instance.GetTexture("Hangar/Icons/"+ICON, false);
+				var _icon_s = GameDatabase.Instance.GetTexture("Hangar/Icons/"+ICON+"_selected", false);
+				var pc_icon = new PartCategorizer.Icon(ICON, _icon, _icon_s, true);
+				PartCategorizer.Instance.icons.Add(pc_icon);
+				PartCategorizer.Instance.iconDictionary.Add(ICON, pc_icon);
+			}
+			var icon = PartCategorizer.Instance.GetIcon(ICON);
+			//add custom function filter
+			var filter = PartCategorizer.Instance.filters
+				.Find(f => f.button.categoryName == "Filter by Function");
+			PartCategorizer.AddCustomSubcategoryFilter(filter, CATEGORY, icon, 
+				p => p.moduleInfos.Any(m => has_hangar(m.moduleName)));
+			//set icon(s) for all the modules
+			PartCategorizer.Instance.filters
+				.Find(f => f.button.categoryName == "Filter by Module")
+				.subcategories.FindAll(s => has_hangar(string.Join("", s.button.categoryName.Split())))
+				.ForEach(c => c.button.SetIcon(icon));
+			//Apparently needed to make sure the icon actually shows at first
+			var button = filter.button.activeButton;
+			button.SetFalse(button, RUIToggleButtonTyped.ClickType.FORCED);
+			button.SetTrue(button, RUIToggleButtonTyped.ClickType.FORCED);
+		}
+	}
 }
 
