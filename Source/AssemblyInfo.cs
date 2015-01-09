ed49609c9d8301e3bc0bc963e970559b64e97e61
Allis Tauri <allista@gmail.com>
2015-01-09 16:10:34 +0300

    Changed version to v2.0.2.2. Added KSPAssembly attribute.

--- Source/AssemblyInfo.cs
+++ Source/AssemblyInfo.cs
@@ -17,7 +17,10 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("2.0.2.1")]
+[assembly: AssemblyVersion("2.0.2.2")]
+[assembly: KSPAssembly("Hangar", 2, 0)]
+//TODO: enable this when KAE pull in my request
+//[assembly: KSPAssemblyDependency("KSPAPIExtensions", 1, 7)]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
