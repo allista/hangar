3712617a90969cb407594c7d6c4044d58c619bd3
Allis Tauri <allista@gmail.com>
2015-01-31 23:43:19 +0300

    Changed download link to KS.

--- Source/AssemblyInfo.cs
+++ Source/AssemblyInfo.cs
@@ -37,7 +37,7 @@ namespace AtHangar
 		public static readonly Version MinKSPVersion = new Version(0,90,0);
 		public static readonly Version MaxKSPVersion = new Version(0,90,0);
 		public static readonly string  VersionURL    = "https://raw.githubusercontent.com/allista/hangar/master/GameData/Hangar/Hangar.version";
-		public static readonly string  UpgradeURL    = "https://github.com/allista/hangar/releases";
+		public static readonly string  UpgradeURL    = "https://kerbalstuff.com/mod/270/Hangar";
 		public static readonly string  ChangeLogURL  = "https://github.com/allista/hangar/blob/master/ChangeLog.md";
 		public static readonly string  VersionFile   = "Hangar.version";
 	}
