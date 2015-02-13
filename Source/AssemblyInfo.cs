ae77b2817af291811b24478474d9d54a09231436
Allis Tauri <allista@gmail.com>
2015-02-13 10:56:31 +0300

    Changed version to 2.2.1

--- Source/AssemblyInfo.cs
+++ Source/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("2.2.0")]
+[assembly: AssemblyVersion("2.2.1")]
 [assembly: KSPAssembly("Hangar", 2, 2)]
 //TODO: enable this when KAE pull in my request
 //[assembly: KSPAssemblyDependency("KSPAPIExtensions", 1, 7)]
