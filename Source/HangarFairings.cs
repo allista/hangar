7b622b12c65145f97f44dd925c8a51b0d38c8f8e
Allis Tauri <allista@gmail.com>
2015-02-13 20:13:58 +0300

    Working flight plane + control state transfer. Closed #130.

--- Source/HangarFairings.cs
+++ Source/HangarFairings.cs
@@ -6,8 +6,6 @@ namespace AtHangar
 {
 	public class HangarFairings : Hangar
 	{
-		[KSPField(isPersistant = true)]
-		public bool staged;
 		bool launch_in_progress;
 
 		[KSPField(isPersistant = true)]
@@ -33,14 +31,14 @@ namespace AtHangar
 
 		IEnumerator<YieldInstruction> update_crew_capacity()
 		{
-			this.Log("starting update_crew_capacity");
+			this.Log("starting update_crew_capacity");//debug
 			if(!HighLogic.LoadedSceneIsEditor) yield break;
 			while(true)
 			{
-				this.Log("updating crew capacity: Storage {0}", Storage);
+				this.Log("updating crew capacity: Storage {0}", Storage);//debug
 				var vsl = Storage != null && Storage.VesselsDocked > 0 ? 
 					Storage.GetConstructs()[0] : null;
-				this.Log("updating crew capacity: vsl {0}, capacity {1}, saved {2}", 
+				this.Log("updating crew capacity: vsl {0}, capacity {1}, saved {2}", //debug
 					vsl, vsl == null? -1 : vsl.CrewCapacity, CrewCapacity);
 				var capacity = vsl != null? vsl.CrewCapacity : 0;
 				if(capacity != part.partInfo.partPrefab.CrewCapacity)
@@ -52,7 +50,7 @@ namespace AtHangar
 		void update_crew_capacity(int capacity)
 		{
 			part.partInfo.partPrefab.CrewCapacity = part.CrewCapacity = CrewCapacity = capacity;
-			this.Log("prefab.CrewCapacity {0}", part.partInfo.partPrefab.CrewCapacity);
+			this.Log("prefab.CrewCapacity {0}", part.partInfo.partPrefab.CrewCapacity);//debug
 			ShipConstruction.ShipManifest = HighLogic.CurrentGame.CrewRoster.DefaultCrewForVessel(ShipConstruction.ShipConfig);
 			CMAssignmentDialog.Instance.RefreshCrewLists(ShipConstruction.ShipManifest, false, true);
 			Utils.UpdateEditorGUI();
@@ -62,6 +60,34 @@ namespace AtHangar
 		{
 			base.start_coroutines();
 			StartCoroutine(update_crew_capacity());
+		}
+
+		protected override void on_vessel_launch(StoredVessel sv)
+		{
+			sv.crew.Clear();
+			sv.crew.AddRange(part.protoModuleCrew);
+			//transfer the target and controls
+			var this_vsl = vessel.BackupVessel();
+			sv.vessel.targetInfo   = this_vsl.targetInfo;
+			sv.vessel.ctrlState    = this_vsl.ctrlState;
+			sv.vessel.actionGroups = this_vsl.actionGroups;
+			//transfer the flight plan
+			if(vessel.patchedConicSolver.maneuverNodes.Count > 0)
+			{
+				var nearest_node = vessel.patchedConicSolver.maneuverNodes[0];
+				var new_orbit = sv.vessel.orbitSnapShot.Load();
+				var vvel = new_orbit.getOrbitalVelocityAtUT(nearest_node.UT).xzy;
+				var vpos = new_orbit.getPositionAtUT(nearest_node.UT).xzy;
+				nearest_node.nodeRotation = Quaternion.LookRotation(vvel, Vector3d.Cross(-vpos, vvel));
+				nearest_node.DeltaV = nearest_node.nodeRotation.Inverse() * (nearest_node.nextPatch.getOrbitalVelocityAtUT(nearest_node.UT).xzy-vvel);
+				sv.vessel.flightPlan.ClearData();
+				vessel.patchedConicSolver.Save(sv.vessel.flightPlan);
+				vessel.patchedConicSolver.maneuverNodes.Clear();
+				vessel.patchedConicSolver.flightPlan.Clear();
+			}
+			//turn fairings off
+			Storage.enabled = Storage.isEnabled = false;
+			enabled = isEnabled = false;
 		}
 
 		IEnumerator<YieldInstruction> delayed_launch()
@@ -75,24 +101,20 @@ namespace AtHangar
 			while(hangar_gates.Playing) yield return null;
 			//activate the hangar, get the vessel from the storage, set its crew
 			Activate();
-			var vsl = Storage.GetVessels()[0];
-			vsl.crew.Clear();
-			vsl.crew.AddRange(part.protoModuleCrew);
+			part.Effect("decouple"); //FIXME: not working
+			yield return new WaitForSeconds(1f);
 			//try to restore vessel and check the result
-			TryRestoreVessel(vsl);
-			staged = Storage.VesselsDocked == 0;
-			Events["LaunchVessel"].active = Actions["LaunchVesselAction"].active = !staged;
-			launch_in_progress = false;
+			TryRestoreVessel(Storage.GetVessels()[0]);
 		}
 
-		[KSPEvent(guiActive = true, guiName = "Jettison Content", guiActiveUnfocused = true, externalToEVAOnly = true, unfocusedRange = 4)]
+		[KSPEvent(guiActive = true, guiName = "Jettison Payload", guiActiveUnfocused = true, externalToEVAOnly = true, unfocusedRange = 4)]
 		public void LaunchVessel()
 		{
-			if(staged || launch_in_progress || hangar_gates == null) return;
+			if(!enabled || launch_in_progress || hangar_gates == null) return;
 			Open();	StartCoroutine(delayed_launch());
 		}
 
-		[KSPAction("Jettison Content")]
+		[KSPAction("Jettison Payload")]
 		public void LaunchVesselAction(KSPActionParam param)
 		{ LaunchVessel(); }
 
