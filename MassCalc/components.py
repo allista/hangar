4645d186d9069d201e4c627cfcbd28182d8f9202
Allis Tauri <allista@gmail.com>
2015-02-28 22:45:15 +0300

    Moved materials to components. Rebalanced reaction_wheels to be surfaces rather then volumes. Closer to stock in balance.
    Also:
    * corrected probe core costs with respect to the new perk system.
    * added reaction_wheel_real that models in-game reaction wheels perfectly, but
    inusable with the current PartResizer module.

--- MassCalc/components.py
+++ MassCalc/components.py
@@ -1,12 +1,20 @@
 from base_classes import volume, surface, material
+import numpy as np
 
-class custom_volume(volume):
+steel      = material(8.05,  2.0)
+aluminium  = material(2.7,   8.0)
+Al_Li      = material(2.63, 12.0)
+composits  = material(1.9,  20.0)
+compositsL = material(1.3,  18.0)
+lavsan     = material(300e-6/0.001, 1)
+
+class _custom_volume(volume):
     _name = 'custom volume'
     _density = 1
     _cost_density = 1
     
-    def __init__(self, V):
-        volume.__init__(self, V, self._name, C=self._cost_density, D=self._density)
+    def __init__(self, vol):
+        volume.__init__(self, vol, self._name, C=self._cost_density, D=self._density)
         
     def _add_str(self): return ''
     
@@ -16,22 +24,121 @@ class custom_volume(volume):
         return s
 #end class
 
-class reaction_wheel(custom_volume):
+
+class reaction_wheel_real(_custom_volume):
+    '''
+    This is an accurate model of the series of the stock reaction wheels.
+    Useless until I implement custom polynomial curves for each parameter.
+    '''
+
     _name = 'reaction wheel'
-    _density = 0.2/0.21
-    _cost_density = 2100/0.21
+    _density = 0.0
+    _cost_density = 0.0
     
-    _spec_torque = 156.25 #torque/t
-    _spec_energy = 3.395  #El.u/sec/t
+    _hd_ratio = 5.0
     
-    def _add_str(self): 
-        m = self.full_mass()
-        s  = '   torque = %.0f\n' % (self._spec_torque * m) 
-        s += '   rate = %.3f\n' % (self._spec_energy * m)
+    @staticmethod
+    def cylV(d, h): return np.pi*d**2/4.0*h
+    
+    @classmethod
+    def cylS(cls, v):
+        d=(v/np.pi*4.0*cls._hd_ratio)**(1/3.0)
+        h=d/cls._hd_ratio
+        return np.pi*d*h
+    
+    @classmethod
+    def _vol2mass(cls, v):
+        return 0.0897259823*v**(2/3.0)+0.0370799071
+    
+    @classmethod
+    def _vol2torque(cls, v):
+        return 19.5152925063*v**0.48
+    
+    @classmethod
+    def _vol2cost(cls, v):
+        return 1170.13502*v**0.48 +300.922491
+    
+    @classmethod
+    def _vol2energy(cls, v):
+        return 1.0295852148*v**(0.1)-0.5253809288
+    
+    def __init__(self, vol):
+        _custom_volume.__init__(self, vol)
+        mat = material(steel.density, self._vol2cost(vol))
+        s = self.cylS(vol)
+        h = self._vol2mass(vol)/mat.density/s
+        mat.cost = mat.cost/s/h*surface.unit_h
+        volume.__init__(self, vol, self._name, C=0, D=0,
+                        S=surface(s, h, mat))
+        
+    @property
+    def torque(self): return self._vol2torque(self._V)
+    
+    @property
+    def energy(self): return self._vol2energy(self._V)
+    
+    def _add_str(self):
+        s  = '   torque = %.0f\n' % (self._vol2torque(self._V)) 
+        s += '   rate = %.3f\n' % (self._vol2energy(self._V))
         return s
 #end class
 
-class solar_panel(custom_volume):
+class reaction_wheel(_custom_volume):
+#     s: 0.05 t / 5 trq / 0.25 ec / 600 cr / 0.0592276609 m3 / D= 0.8442001463542519
+#     m: 0.1 t / 15 trq / 0.45 ec / 1200 cr / 0.572555261117 m3 / D= 0.1746556302790925 / S = 1.48814376854
+#     l: 0.2 t / 30 trq / 0.6 ec / 2100 cr / 2.45436926062 m3 / D= 0.08148733086295168
+    
+    _name = 'reaction wheel'
+    _density = 0
+    _cost_density = 0
+    
+    _spec_torque = 150 #torque/t
+    _spec_energy = 4.5  #El.u/sec/t
+    
+    _thickness = 0.05
+    _material = material(0.1/1.48814376854/_thickness, 
+                         1200/1.48814376854/_thickness*surface.unit_h)
+    
+    _hd_ratio = 5.0
+    
+    @staticmethod
+    def cylV(d, h): return np.pi*d**2/4.0*h
+    
+    @classmethod
+    def cylS(cls, v):
+        d = (v/np.pi*4.0*cls._hd_ratio)**(1/3.0)
+        h = d/cls._hd_ratio
+        return np.pi*d*h
+    
+    @classmethod
+    def S2V(cls, s):
+        d = np.sqrt(s*cls._hd_ratio/np.pi)
+        return cls.cylV(d, d/cls._hd_ratio)
+    
+    def __init__(self, **kwargs):
+        V = kwargs.get('V', -1.0)
+        T = kwargs.get('T', -1.0)
+        if V < 0 and T < 0: 
+            raise ValueError("%s: either volume or torque should be provided." % self._name)
+        if V < 0: V = self.S2V(T/(self._thickness*self._material.density*self._spec_torque))
+        _custom_volume.__init__(self, V)
+        volume.__init__(self, V, self._name, C=0, D=0,
+                        S=surface(self.cylS(V), self._thickness, self._material))
+         
+    @property
+    def torque(self): return self._spec_torque*self.full_mass()
+     
+    @property
+    def energy(self): return self._spec_energy*self.full_mass()
+     
+    def _add_str(self):
+        m = self.full_mass()
+        s  = '   torque = %.0f\n' % (self._spec_torque*m) 
+        s += '   rate = %.3f\n' % (self._spec_energy*m)
+        return s
+#end class
+
+class solar_panel(_custom_volume):
     _name = 'solar panels'
     _density = 0
     _cost_density = 0
@@ -42,14 +149,14 @@ class solar_panel(custom_volume):
     
     def __init__(self, S):
         self.energy = S*self._surface_energy
-        custom_volume.__init__(self, S*self._thickness)
+        _custom_volume.__init__(self, S*self._thickness)
         self._surface = surface(S, self._thickness, self._material)
         
     def _add_str(self):
         return '   chargeRate = %.3f\n' % self.energy
 #end class
 
-class battery(custom_volume):
+class battery(_custom_volume):
     #stock densities:
     #(0.005/0.033, 0.01/0.083, 0.02/0.1, 0.05/0.3, 0.2/1.6) = [0.15151515, 0.12048193, 0.2, 0.16666667, 0.125]
     #stock costs per volume:
@@ -70,9 +177,9 @@ class battery(custom_volume):
         V = kwargs.get('V', -1.0)
         E = kwargs.get('E', -1.0)
         if V < 0 and E < 0: 
-            raise ValueError("%s: either V or energy should be positive" % self._name)
+            raise ValueError("%s: either volume or energy should be provided" % self._name)
         #compute energy, energy density and volume
-        if        V < 0: V = E/self._energy_density
+        if   V < 0: V = E/self._energy_density
         elif E < 0: E = V*self._energy_density
         else: 
             self._energy_density = E/float(V)
@@ -82,7 +189,7 @@ class battery(custom_volume):
         self._cost_density = self._energy_cost*E/V
         self.energy = E
         #initialize volume
-        custom_volume.__init__(self, V)
+        _custom_volume.__init__(self, V)
     #end def
     
     def _add_str(self):
--- MassCalc/main.py
+++ MassCalc/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 
 from base_classes import material, surface, volume, part
-from components import battery, generator, reaction_wheel, solar_panel
+from components import *
 
 
 def format_data(x, ys, w=None):
@@ -22,13 +22,6 @@ def format_data(x, ys, w=None):
 if __name__ == '__main__':
     scales = np.arange(0.5, 4.1, 0.5)
     
-    steel      = material(8.05,  2.0)
-    aluminium  = material(2.7,   8.0)
-    Al_Li      = material(2.63, 12.0)
-    composits  = material(1.9,  20.0)
-    compositsL = material(1.3,  18.0)
-    lavsan     = material(300e-6/0.001, 1)
-
     inline1   = part('InlineHangar',
                      [volume(9.4, 'hull', 
                              C=1, D=0.02, 
@@ -46,7 +39,7 @@ if __name__ == '__main__':
                              V=[volume(53.66, 'top compartment', C=20, D=0.05,
                                        V=[volume(7.34, 'cabins', C=4000, M=0.35*3, N=2,
                                                  S=surface(25.19, 0.003, Al_Li)),
-                                          volume(15.5, 'coridor', C=2, D=0.0012,
+                                          volume(15.5, 'corridor', C=2, D=0.0012,
                                                  S=surface(41.33, 0.003, Al_Li)),
                                           volume(9.17, 'machinery', C=1900, M=1.2)])]),
                       volume(4.05, 'doors', C=1, D=0.02, N=2,
@@ -60,32 +53,32 @@ if __name__ == '__main__':
                              S=surface(960.55, 0.007, composits),
                              V=[volume(46.92, 'machinery room', C=3500, M=4,
                                        V=[battery(E=20000),
-                                          reaction_wheel(0.95),
+                                          reaction_wheel(T=140),
                                           generator(E=6.75),
                                           volume(2, 'monopropellent tank', 
                                                  S=surface(11.04, 0.003, Al_Li))]),
                               volume(112.64, 'side-space', C=20, D=0.05, N=2,
                                      V=[volume(2.88, 'cabins', C=2000, M=0.35, N=5,
                                                S=surface(12.8, 0.01, compositsL)),
-                                        volume(27.25, 'coridors', C=1, D=0.0012,
+                                        volume(27.25, 'corridors', C=1, D=0.0012,
                                                S=surface(93.66, 0.01, compositsL)),
                                         volume(8.79, 'doors machinery', C=800, M=0.35,
                                                S=surface(26.44, 0.01, compositsL))]),
-                              volume(1.5*2+8.7, 'coridors', C=1, D=0.0012)
+                              volume(1.5*2+8.7, 'corridors', C=1, D=0.0012)
                               ]),
                       volume(1.64, 'doors', C=2, D=0.01, N=2,
                              S=surface(28.94, 0.007, composits)),
                               ],
                      add_mass=4+0.08, #cockpit, probe core
-                     add_cost=980 + 300 + 4000 + 600,  #DockPort + Light + Cockpit + probe core
+                     add_cost=980 + 300 + 4000 + 3400,  #DockPort + Light + Cockpit + probe core
                      res_cost=2400) #Monoprop
 
     lander     = part('RoverLander', 
                       [volume(9.536, 'hull', C=2, D=0.01,
                               S=surface(92.1, 0.004, Al_Li),
-                              V=[volume(3.498, 'machinery', C=920, M=0.35),
-                                 volume(2.225, 'base', C=40, D=0.15,
-                                        V=[reaction_wheel(0.17)])]),
+                              V=[volume(3.498, 'machinery', C=920, M=0.30),
+                                 volume(2.225, 'base', C=40, D=0.3,
+                                        V=[reaction_wheel(T=25)])]),
                        volume(0.58+0.0138*2, 'doors', C=1, D=0.02, N=2,
                               S=surface(13.9, 0.003, Al_Li),
                               V=[volume(0.0138, 'ramp side walls', N=2, material=Al_Li),]),
@@ -104,8 +97,9 @@ if __name__ == '__main__':
                        volume(0.002, 'hinges', N=8, material=aluminium),
                        ],
                      add_mass=0.04, #probe core
-                     add_cost=200 + 480, #Light + probe core
-                     res_cost=324 + 89.1 + 240) #LF+Ox+MP
+                     add_cost=200 + 1480, #Light + probe core
+                     res_cost=324 + 89.1 + 240, #LF+Ox+MP
+                     size=2)
 
     #ground hangars
     small     = part('SmallHangar',
@@ -119,7 +113,7 @@ if __name__ == '__main__':
                       volume(0.18,'clamp', C=300, D=0.78)
                               ], 
                      add_mass=0.04, #probe core
-                     add_cost=100 + 480) #Light + probe core
+                     add_cost=100 + 300) #Light + probe core
     
     big       = part('BigHangar',
                      [volume(377.84, 'hull', C=2, D=0.01,
@@ -127,7 +121,7 @@ if __name__ == '__main__':
                              V=[volume(218.99, 'rear-compartment', C=150, D=0.02,
                                        V=[volume(12.126, 'cabins', C=3000, M=1, N=6,
                                                  S=surface(31.88, 0.01, composits)),
-                                          volume(11.12, 'coridors', C=150, D=0.0012, N=3,
+                                          volume(11.12, 'corridors', C=150, D=0.0012, N=3,
                                                  S=surface(40.6, 0.01, composits)),
                                           volume(67.57, 'machinery', C=4260, M=5.4, 
                                                  V=[battery(V=20, E=40000),
@@ -137,7 +131,7 @@ if __name__ == '__main__':
                       volume(4.34, 'clamp', C=300, D=0.78),
                               ],
                      add_mass=0.04, #probe core
-                     add_cost=300 + 480) #Light + probe core
+                     add_cost=300 + 300) #Light + probe core
     
     #inflatables
     inflatable1 = part('InflatableHangar1',
@@ -153,7 +147,7 @@ if __name__ == '__main__':
                              S=surface(15.06, 0.001, lavsan)),
                               ], 
                      add_mass=0.04, #probe core
-                     add_cost=480) #probe core
+                     add_cost=300) #probe core
     
     inflatable2 = part('InflatableHangar2',
                      [volume(0.444, 'hull', C=1, D=0.02,
@@ -173,7 +167,7 @@ if __name__ == '__main__':
                              S=surface(15.06, 0.001, lavsan)),
                               ], 
                      add_mass=0.04, #probe core
-                     add_cost=480) #Light + probe core
+                     add_cost=300) #probe core
     
     mk3_hangar   = part('Mk3Hangar',
                      [volume(130.43, 'hull', C=100, D=0.02,
@@ -181,7 +175,7 @@ if __name__ == '__main__':
                              V=[volume(5, 'machinery', C=1200, M=0.7),
                                 generator(E=1.5),
                                 battery(E=5000),
-                                reaction_wheel(0.8),
+                                reaction_wheel(T=120),
                                 volume(0.011, 'hydraulics', N=2, 
                                        material=aluminium),
                                 volume(100, 'tanks', C=20)]),
@@ -192,20 +186,19 @@ if __name__ == '__main__':
                      add_cost=0)
     
     fairings_octo = part('FairingsOcto',
-                     [volume(0.36, 'hull', C=1, D=0.02,
-                             S=surface(20.6 , 0.001, Al_Li)),
-                      volume(0.74, 'base', C=1, D=0.02,
-                             S=surface(2.23, 0.003, Al_Li),
-                             V=[reaction_wheel(0.1),
-                                battery(E=100),
-                                volume(0.1, 'jettison-charge', C=400, M=0.05)]),
-                      volume(0.01, 'petals', N=8,
-                             S=surface(1.02, 0.002, composits)),
+                     [volume(1.11, 'hull', C=1, D=0.02,
+                             S=surface(35.398 , 0.001, Al_Li),
+                             V=[volume(0.427, 'base', C=1, D=0.02,
+                                       S=surface(4.543, 0.002, Al_Li),
+                                       V=[
+                                          reaction_wheel(T=10),
+                                          battery(E=50),
+                                          volume(0.1, 'jettison-charge', C=400, M=0.05)])]),
+                      volume(0.149, 'petals', N=2,
+                             S=surface(6.096, 0.002, composits)),
                       ], 
-                     add_mass=0,
-                     add_cost=0)
-    
-    sys.exit()
+                     add_mass=0.04,
+                     add_cost=450)
     
     #utilities
     adapter  = part('Adapter', 
@@ -358,8 +351,8 @@ if __name__ == '__main__':
                                   S=surface(4.93, 0.006, Al_Li)),
                            volume(2.3, 'S2-port', C=100, D=0.1, 
                                   S=surface(13.62, 0.006, Al_Li),
-                                  V=[battery(E=4000),
-                                     reaction_wheel(V=0.316)])
+                                  V=[battery(E=2000),
+                                     reaction_wheel(T=32)])
                            ],
                           add_cost=400) #light
     
@@ -380,7 +373,7 @@ if __name__ == '__main__':
                                  V=[volume(120, 'machinery', C=8350, M=12),
                                     volume(10, 'rock-tank', C=20),
                                     volume(5, 'rcs-tank', C=20),
-                                    reaction_wheel(1.15),
+                                    reaction_wheel(V=7),
                                     generator(E=15)]),
                           volume(43.08, 'main-drill', C=23780, M=6,
                                  S=surface(81.66, 0.005, steel)),
@@ -390,13 +383,13 @@ if __name__ == '__main__':
                           battery(V=1.45*4),
                           ]+asteroid_port.volumes,
                          add_mass=0.04, #probe core
-                         add_cost=480) #probe core
+                         add_cost=3400) #probe core
     
     asteroid_gateway = part('AsteroidGateway', 
                          [volume(24.24, 'hull', C=100, D=0.1,
                                  S=surface(219.04, 0.007, Al_Li),
                                  V=[battery(E=10000),
-                                    reaction_wheel(0.55),
+                                    reaction_wheel(V=6.0),
                                     generator(E=6.0),
                                     volume(5, 'monopropellent tank', C=20)]),
                           volume(1.54, 'doors', C=2, D=0.01, N=2,
@@ -417,7 +410,7 @@ if __name__ == '__main__':
                                     ]),
                           ]+asteroid_port.volumes,
                          add_mass=0.08, #probe core
-                         add_cost=600 + 600,  #Light + probe core
+                         add_cost=600 + 3400,  #Light + probe core
                          res_cost=0)
     
     ore_converter = part('RockOreConverter',
