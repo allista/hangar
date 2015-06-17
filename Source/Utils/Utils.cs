37d0bcc9c098942f595c9416e25b8061acb8d1fc
Allis Tauri <allista@gmail.com>
2015-06-17 21:15:52 +0300

    Added ConfigNodeWrapper to serialize ConfigNodes.
    Why the hell has Squad desided to make ConfigNode non-serializable is a mistery
    to me >_<

--- Source/Utils/Utils.cs
+++ Source/Utils/Utils.cs
@@ -12,6 +12,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using KSP.IO;
 
 namespace AtHangar
 {
@@ -242,6 +243,50 @@ namespace AtHangar
 		{
 			Utils.DelayPhysicsForSeconds(TimeWarp.deltaTime);
 			return null;
+		}
+	}
+
+	//fuck you Squad! How come the ConfigNode is not serializable anymore? O_o
+	[System.Serializable]
+	public class ConfigNodeWrapper
+	{
+		public string name;
+
+		public ConfigNode.ValueList values;
+		public List<ConfigNodeWrapper> nodes = new List<ConfigNodeWrapper>();
+
+		public ConfigNodeWrapper(ConfigNode node)
+		{
+			name = node.name;
+			values = node.values;
+			foreach(ConfigNode n in node.nodes)
+				nodes.Add(new ConfigNodeWrapper(n));
+		}
+
+		public ConfigNode ToConfigNode()
+		{
+			var node = new ConfigNode(name);
+			foreach(ConfigNode.Value v in values) 
+				node.AddValue(v.name, v.value);
+			foreach(var n in nodes)
+				node.AddNode(n.ToConfigNode());
+			return node;
+		}
+
+		public static byte[] SaveConfigNode(ConfigNode node)
+		{ 
+			byte[] data = null;
+			try { data = IOUtils.SerializeToBinary(new ConfigNodeWrapper(node)); }
+			catch(System.Exception ex) { Utils.Log("{0}\n{1}\n{2}", ex.Message, ex.Source, ex.StackTrace); }
+			return data;
+		}
+
+		public static ConfigNode RestoreConfigNode(byte[] data)
+		{ 
+			ConfigNodeWrapper node = null;
+			try { node = IOUtils.DeserializeFromBinary(data) as ConfigNodeWrapper; }
+			catch(System.Exception ex) { Utils.Log("{0}\n{1}\n{2}", ex.Message, ex.Source, ex.StackTrace); }
+			return node != null? node.ToConfigNode() : null;
 		}
 	}
 }
