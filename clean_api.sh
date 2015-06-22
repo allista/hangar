344fea9fe0f8cb0c48f5d869ed99b55ae99bfb8c
Allis Tauri <allista@gmail.com>
2015-06-22 09:53:21 +0300

    Made clean_api.sh work with the current API.

--- clean_api.sh
+++ clean_api.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 cd $(dirname "$0")
+pwd
 
 API=KSP_API
+echo "Cleaning .cs files in $API"
 
-#remove while(true)
-find $API -name "*.cs" -print0 | xargs -0 perl -i -0pe 's/while \(true\)\n\s*{\n\s*switch \(\d\)\n\s*{\n\s*case 0:\n\s*continue;\n\s*}\n\s*break;\n\s*}//mg'
+files="$(find "$API" -name "*.cs")"
+[[ -n "$files" ]] || { echo "No .cs files was found"; exit; }
 
-#remove if(!true)
-find $API -name "*.cs" -print0 | xargs -0 perl -i -0pe 's/if \(\!true\)\n\s*{\n\s*RuntimeMethodHandle .*\n\s*}//mg'
-
-#remove empty lines
-find $API -name "*.cs" -print0 | xargs -0 perl -i -0pe 's/^\s*\n//mg'
+for f in $files; do
+	before=$(ls --block-size=1 -s "$f")
+	#remove 'while (true) {}' blocks
+	perl -i -0pe 's/\s*while \(true\)\r*\n\s*{\r*\n\s*switch \(\d\)\r*\n\s*{\r*\n\s*case 0:\r*\n\s*continue;\r*\n\s*}\r*\n\s*break;\r*\n\s*}//mg' "$f"
+	#remove 'if (!true) {}' blocks
+	perl -i -0pe 's/\s*if \(\!true\)\r*\n\s*{(\r*\n\s*[^}].*\r*\n)+\s*}//mg' "$f"
+	#remove empty lines
+	perl -i -0pe 's/^\s*\r*\n//mg' "$f"
+	#check the result
+	after=$(ls --block-size=1 -s "$f")
+	[[ "$before" == "$after" ]] || { echo "Cleaned $f:"; echo $before; echo $after; }
+done
