a9a3072127eb1c479e171cf3070d00ac3a5c9051
Allis Tauri <allista@gmail.com>
2016-10-15 22:42:20 +0300

    Excluded AnimatedConverters and CC/Parts from distributive.

--- make-release.sh
+++ make-release.sh
@@ -4,4 +4,6 @@ cd $(dirname "$0")
 
 ../../PyKSPutils/make_mod_release  -s 'Source' \
 -e '*/ForModders/*' '*/config.xml' '*.user' '*.orig' '*.mdb' \
+'GameData/000_AT_Utils/Plugins/AnimatedConverters.dll' \
+'GameData/ConfigurableContainers/Parts/*' \
 -i '../AT_Utils/GameData' '../AT_Utils/ConfigurableContainers/GameData'
