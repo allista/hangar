070f1b48958fd319e0956fbed238577ede65cfdf
Allis Tauri <allista@gmail.com>
2016-12-20 00:05:10 +0300

    Changed version to 3.2.0

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -18,8 +18,8 @@ using AT_Utils;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: KSPAssembly("Hangar", 3, 0)]
+[assembly: AssemblyVersion("3.2.0.0")]
+[assembly: KSPAssembly("Hangar", 3, 2)]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
@@ -34,8 +34,8 @@ namespace AtHangar
 	{
 		public ModInfo()
 		{
-			MinKSPVersion = new Version(1,2,1);
-			MaxKSPVersion = new Version(1,2,1);
+			MinKSPVersion = new Version(1,2,2);
+			MaxKSPVersion = new Version(1,2,2);
 
 			VersionURL   = "https://raw.githubusercontent.com/allista/hangar/master/GameData/Hangar/Hangar.version";
 			UpgradeURL   = "https://github.com/allista/hangar/releases";
