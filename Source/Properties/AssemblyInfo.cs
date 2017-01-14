8b371eaea939c8b5d0010805ebf3e90e49946edd
Allis Tauri <allista@gmail.com>
2017-01-15 00:28:22 +0300

    Changed version to 3.2.1.3

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using AT_Utils;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("3.2.1.2")]
+[assembly: AssemblyVersion("3.2.1.3")]
 [assembly: KSPAssembly("Hangar", 3, 2)]
 
 // The following attributes are used to specify the signing key for the assembly, 
