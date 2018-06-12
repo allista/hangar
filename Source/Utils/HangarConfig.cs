0c0c75c723b499fd425c8da67816f4a89c8384cb
Allis Tauri <allista@gmail.com>
2018-06-12 13:21:41 +0300

    Moved MeshesToSkip to AT_Utils

--- Source/Utils/HangarConfig.cs
+++ Source/Utils/HangarConfig.cs
@@ -31,8 +31,6 @@ namespace AtHangar
         [Persistent] public float  MaxSqrSurfaceVelocity = 0.01f; //m/s
         [Persistent] public float  MaxGeeForce           = 0.2f;  //g
         [Persistent] public float  MaxStaticPressure     = 0.01f; //atm
-        //for Metric
-        [Persistent] public string MeshesToSkip = string.Empty;
         //misc
         [Persistent] public string KethaneMapCollider  = "MapOverlay collider";
         [Persistent] public bool   UseStockAppLauncher = false;
@@ -41,11 +39,8 @@ namespace AtHangar
 
         public override void Init()
         { 
-            //init meshes names
-            if(!string.IsNullOrEmpty(MeshesToSkip))
-                Metric.MeshesToSkip.AddUniqueRange(Utils.ParseLine(MeshesToSkip, Utils.Comma));
             ResourcesBlacklist = string.IsNullOrEmpty(DontCloneResources)?
-                new string[0] : Utils.ParseLine(DontCloneResources, Utils.Comma);
+                                       new string[0] : Utils.ParseLine(DontCloneResources, Utils.Comma);
         }
     }
 }
