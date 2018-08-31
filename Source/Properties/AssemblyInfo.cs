fc27772c5be576fecc7493e4063db5b159de43a7
Allis Tauri <allista@gmail.com>
2018-08-31 21:39:48 +0300

    Changed version to 3.3.4.3

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using AT_Utils;
 #if NIGHTBUILD
 [assembly: AssemblyVersion("3.3.*")]
 #else
-[assembly: AssemblyVersion("3.3.4.2")]
+[assembly: AssemblyVersion("3.3.4.3")]
 #endif
 [assembly: KSPAssembly("Hangar", 3, 3)]
 
