6eb5b573b0ce57d71e22885b9f6adbf27a2c78a0
Allis Tauri <allista@gmail.com>
2020-04-28 07:49:59 +0300

    AssemblyVersion

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using AT_Utils;
 #if NIGHTBUILD
 [assembly: AssemblyVersion("3.5.*")]
 #else
-[assembly: AssemblyVersion("3.5.0.1")]
+[assembly: AssemblyVersion("3.5.0.2")]
 #endif
 [assembly: KSPAssembly("Hangar", 3, 5)]
 
