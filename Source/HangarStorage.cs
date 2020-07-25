60cd3f9b0f1249b7c2b235037a9009b4667235e7
Allis Tauri <allista@gmail.com>
2020-07-25 18:56:56 +0300

    Fixed lag in Editor when opening PAW of hangars in symmetry group
    HangarMachinery.Setup doesn't need to fire onEditorShipModified, nor does it need to update PAW anymore.
    So the on_set_part_params is removed; as well as the call of the part.UpdatePartMenu.

--- Source/HangarStorage.cs
+++ Source/HangarStorage.cs
@@ -1,4 +1,4 @@
-﻿//   HangarStorage.cs
+//   HangarStorage.cs
 //
 //  Author:
 //       Allis Tauri <allista@gmail.com>
@@ -174,21 +174,12 @@ namespace AtHangar
             set_part_params(reset);
         }
 
-        virtual protected void on_set_part_params()
-        {
-            var el = EditorLogic.fetch;
-            if(el != null) GameEvents.onEditorShipModified.Fire(el.ship);
-            //            else if(part.vessel != null) GameEvents.onVesselWasModified.Fire(part.vessel);
-        }
-
-        virtual protected void set_part_params(bool reset = false)
+        protected virtual void set_part_params(bool reset = false)
         {
             _stored_vessels = VesselsCount.ToString();
             _stored_mass = Utils.formatMass(VesselsMass);
             _stored_cost = VesselsCost.ToString();
             _used_volume = Volume > 0 ? UsedVolumeFrac.ToString("P1") : "N/A";
-            on_set_part_params();
-            part.UpdatePartMenu();
         }
 
         public override void OnAwake()
